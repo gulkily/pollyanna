900af9194ee872342470fb0fdf3459874559bdd9
Your Name <you@example.com>
2022-07-21 14:10:55 -0400

    make rebuild.sh exit if more than 100 data files

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+txtCount=$(find html/txt -type f | wc -l) #Count files and store in a variable
+#if ((txtCount -ge 10000));
+if [ "$txtCount" -ge 100 ];
+	then
+		echo ===
+		echo Refusing to rebuild because more than 100 files of data
+		echo Use --override or -O to override NOT IMPLEMENTED YET
+		echo ===
+		exit
+fi
+
 ./clean.sh
 sleep 1
 ./build.sh
