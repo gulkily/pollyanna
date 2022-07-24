34b5b67b79dc174f01c03765c20ebde508acc333
Your Name <you@example.com>
2022-07-24 02:46:51 -0400

    add skeleton for chain.pl and random.pl

---
+++ default/template/perl/page/chain.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -T
+
+use strict;
+use warnings;
+use 5.010;
+
+sub GetChainPage {
+	return 'chain page placeholder'
+}
+
+1;
---
+++ default/template/perl/page/random.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -T
+
+use strict;
+use warnings;
+
+use 5.010;
+
+sub GetRandomPage {
+	return 'random page placeholder';
+}	
+
+1;
