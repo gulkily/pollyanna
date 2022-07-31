5676d4136e2cb9115282fd17a27112ec0c6c1051
Your Name <you@example.com>
2022-07-31 11:13:14 -0400

    move IndexFile() to index_file.pl

---
+++ default/template/perl/index_file.pl
@@ -0,0 +1,228 @@
+#!/usr/bin/perl -T
+
+use strict;
+use warnings;
+use 5.010;
+
+sub IndexFile { # $file ; calls IndexTextFile() or IndexImageFile() based on extension
+    my $file = shift;
+
+    my $flagsReference = shift;
+    my %flags;
+    if ($flagsReference) {
+        %flags = %{$flagsReference};
+    }
+
+    if (!$file) {
+        WriteLog('IndexFile: warning: $file was FALSE; caller = ' . join(',', caller));
+        return '';
+    }
+
+    if ($file eq 'flush') {
+        WriteLog('IndexFile: flush was requested');
+        IndexImageFile('flush');
+        IndexTextFile('flush');
+        return '';
+    }
+
+    if (!$file) {
+        WriteLog('IndexFile: warning: $file is FALSE');
+        return '';
+    }
+
+    chomp $file;
+
+    WriteLog('IndexFile: $file = ' . $file);
+    if (!-e $file) {
+        WriteLog('IndexFile: warning: -e $file is false (file does not exist)');
+        return '';
+    }
+    if (-d $file) {
+        WriteLog('IndexFile: warning: -d $file was true (file is a directory)');
+        return '';
+    }
+
+    #this causes bug
+    #	if ($file =~ m/([0-9a-f]{40})/) {
+    #		#attempted optimization of next block
+    #		my $fileHash = $1;
+    #		if (-e "./cache/b/indexed/$fileHash" || GetCache("indexed/$fileHash")) {
+    #			WriteLog('IndexFile: aleady indexed, returning. $fileHash = ' . $fileHash);
+    #			return $fileHash;
+    #		}
+    #	}
+
+    my $fileHashFromFilename = '';
+    if ($file =~ m/([0-9a-f]{40})/) {
+        my $simpleHash = $1;
+        my $cachedFilename = GetCache('indexed/' . $1);
+
+        if ($cachedFilename) {
+            WriteLog('IndexFile: found hash in filename; $simpleHash = ' . $simpleHash);
+            WriteLog('IndexFile: found hash in filename; $cachedFilename = ' . $cachedFilename);
+            WriteLog('IndexFile: found hash in filename; $file = ' . $file);
+
+            if ( index( $cachedFilename , $file ) != -1 ) {
+                WriteLog('IndexFile: found hash in filename: already indexed, returning. $simpleHash = ' . $simpleHash);
+                return $simpleHash;
+            } else {
+                WriteLog('IndexFile: found hash in filename: NOT indexed, continuing. $simpleHash = ' . $simpleHash);
+            }
+        }
+    }
+
+    my $fileHash = GetFileHash($file);
+
+    if (GetCache('indexed/' . $fileHash)) {
+        if (trim(GetCache('indexed/' . $fileHash)) eq $file) {
+            WriteLog('IndexFile: already indexed, returning. $fileHash = ' . $fileHash);
+            return $fileHash;
+        } else {
+            #return $fileHash; #who does that?#todo
+            WriteLog('IndexFile: already indexed, but from different path. continuing. $fileHash = ' . $fileHash);
+            if (GetConfig('admin/organize_files') && !$flags{'skip_organize'}) {
+                WriteLog('IndexFile: calling OrganizeFile() with $fileHash = ' . $fileHash);
+                $file = OrganizeFile($file);
+            }
+        }
+    }
+
+    my $indexSuccess = 0;
+
+    my $ext = lc(GetFileExtension($file));
+
+    # THREADS MODE IS NOT FINISHED
+    # DO NOT CHANGE THIS UNLESS YKWYD
+    my $useThreads = 0;
+    # IF TURNING ON, UNCOMMENT 'use' STATEMENT
+    # AT THE TOP OF THIS FILE AS WELL
+
+    if ($ext eq 'txt') {
+        WriteLog('IndexFile: calling IndexTextFile()');
+
+        if ($useThreads) {
+            my $thr = threads->create('IndexTextFile', $file);
+            $indexSuccess = $thr->join();
+            $indexSuccess = 1;
+        } else {
+            $indexSuccess = IndexTextFile($file); #IndexFile()
+        }
+
+        if (!$indexSuccess) {
+            WriteLog('IndexFile: warning: IndexTextFile: $indexSuccess was FALSE');
+            $indexSuccess = 0;
+        }
+
+        if (GetConfig('setting/admin/index/add_dir_as_hashtag')) {
+            my $fileLocalPath = $file;
+            $fileLocalPath = str_replace(GetDir('txt'), '', $file);
+            $fileLocalPath = substr($fileLocalPath, 0, rindex($fileLocalPath, '/')); #just the path
+            my @dirTags = split('/', $fileLocalPath);
+            if (@dirTags) {
+                foreach my $dirTag (@dirTags) {
+                    $dirTag = trim($dirTag);
+                    if ($dirTag =~ m/([0-9a-zA-Z_-]+)/ && length($dirTag) > 2) {
+                        $dirTag = $1;
+                        DBAddVoteRecord($fileHash, 0, $dirTag);
+                    } else {
+                        WriteLog('IndexFile: warning: $dirTag failed sanity check; $dirTag = ' . $dirTag);
+                    }
+                }
+            }
+
+            WriteLog('IndexFile: add_dir_as_hashtag: $fileLocalPath = ' . $fileLocalPath);
+
+        }
+
+    }
+
+    if (0 && $ext eq 'html') { #todo enable once IndexHtmlFile() is better
+        WriteLog('IndexFile: calling IndexHtmlFile()');
+        $indexSuccess = IndexHtmlFile($file);
+
+        if (!$indexSuccess) {
+            WriteLog('IndexFile: warning: IndexHtmlFile $indexSuccess was FALSE');
+            $indexSuccess = 0;
+        }
+    }
+
+    if (
+        $ext eq 'png' ||
+            $ext eq 'gif' ||
+            $ext eq 'jpg' ||
+            $ext eq 'jpeg' ||
+            $ext eq 'bmp' ||
+            $ext eq 'svg' ||
+            $ext eq 'webp' ||
+            $ext eq 'jfif' ||
+            $ext eq 'tiff' ||
+            $ext eq 'tff'
+    ) {
+        WriteLog('IndexFile: calling IndexImageFile()');
+        $indexSuccess = IndexImageFile($file);
+    }
+
+    if ($indexSuccess) {
+        WriteLog('IndexFile: $indexSuccess = ' . $indexSuccess);
+    } else {
+        WriteLog('IndexFile: warning: $indexSuccess FALSE; $file = ' . $file . '; caller = ' . join(',', caller));
+    }
+
+    if ($indexSuccess) {
+        if (-e $file) {
+            if (GetConfig('admin/index/stat_file')) { #todo put all the other pieces of this here
+                my @fileStat = stat($file);
+                my $fileSize =    $fileStat[7]; #file size
+                my $fileModTime = $fileStat[9];
+                WriteLog('IndexFile: $fileModTime = ' . $fileModTime . '; $fileSize = ' . $fileSize);
+                if ($fileModTime) {
+                    if (IsItem($indexSuccess)) {
+                        DBAddItemAttribute($indexSuccess, 'file_m_timestamp', $fileModTime);
+                        DBAddItemAttribute($indexSuccess, 'file_size', $fileSize);
+                    } else {
+                        WriteLog('IndexFile: warning: IsItem($indexSuccess) was FALSE');
+                    }
+                }
+            }
+
+            if (GetConfig('admin/index/index_local_path_as_attribute')) {
+                use Cwd 'abs_path';
+                my $absPath = abs_path($file);
+
+                if ($file) {
+                    DBAddItemAttribute($indexSuccess, 'local_path', $absPath);
+                } else {
+                    WriteLog('IndexFile: warning: tried to get $absPath, got FALSE; $file = ' . $file . '; caller = ' . join(',', caller));
+                }
+            }
+
+            if (GetConfig('admin/index/add_git_hash_file')) {
+                #todo sanity check before running shell command #security
+                if ($file =~ m/^([0-9a-z.\/_]+)/) {
+                    $file = $1;
+
+                    my $gitHash = `git hash-object $file`;
+                    if ($gitHash) {
+                        DBAddItemAttribute($indexSuccess, 'git_hash_object', $gitHash);
+                    } else {
+                        WriteLog('IndexFile: warning: $gitHash returned false');
+                    }
+                } else {
+                    WriteLog('IndexFile: warning: add_git_hash_file, $file failed sanity check');
+                }
+            }
+        }
+    }
+
+    if ($indexSuccess) {
+        PutCache('indexed/' . $indexSuccess, $file);
+        return $fileHash;
+    } else {
+        WriteLog('IndexFile: warning: $indexSuccess is FALSE; $file = ' . $file);
+        return '';
+    }
+
+    return 0;
+} # IndexFile()
+
+1;
