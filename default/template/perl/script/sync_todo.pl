2bb7fa000198e526c7a7c114ae6cd0f3b9692346
Your Name <you@example.com>
2022-07-31 12:42:13 -0400

    sync_todo.pl basic

---
+++ default/template/perl/script/sync_todo.pl
@@ -0,0 +1,43 @@
+print "this script is not finished";
+exit;
+
+require('./utils.pl');
+require_once('index.pl');
+print IndexFile();
+
+# if ( condition ) { # probably newest item is newer than todo.txt ? or manually activated
+	# put items back into todo.txt
+#} else {
+	# write todo.txt out into items
+
+	my $todo = GetFile('doc/todo.txt');
+	my @todoArray = split("\n\n", $todo);
+	my $i = 0;
+
+	my $todoReply = "";
+
+	for my $todoItem (@todoArray) {
+		$i++;
+
+		my $fileContent =
+			"#$i\n"
+			.
+			"$todoItem\n"
+			#.
+			#. "===\n"
+		;
+
+		PutFile("html/txt/todo/$i.txt", $fileContent);
+		$newFileHash = IndexFile("html/txt/todo/$i.txt");
+		if ($newFileHash) {
+			DBAddVoteRecord($newFileHash, 0, 'todo');
+			print $newFileHash;
+		} else {
+			exit;
+		}
+
+		#$newFileHash = GetFileHash("html/txt/todo/$i.txt");
+	}
+
+	#call index files
+#}
