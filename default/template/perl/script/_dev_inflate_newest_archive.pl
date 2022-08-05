7968658e94d9431c2e714a9b3cde76581327d601
Your Name <you@example.com>
2022-08-04 20:48:32 -0400

    improve _dev_inflate_newest_archive.pl

--- default/template/perl/script/_dev_inflate_newest_archive.pl
+++ default/template/perl/script/_dev_inflate_newest_archive.pl
@@ -9,51 +9,51 @@ use 5.010;
 
 require './utils.pl';
 
-my @files = `ls -1 archive/*.tar.gz`;
+my @files = `ls -1 archive/*.tar.gz | sort | tail -n 1`;
 
-my $filesNeededCount = 0;
-
-print "Counting files...\n";
-
-for my $file (@files) {
-	chomp $file;
-	if ($file =~ m/([\S]+)/) { #todo more sanity
-		$file = $1;
-		my @filesListed = `tar --list -f $file`;
-		my $filesNeeded = '';
-		for my $fileListed (@filesListed) {
-			chomp $fileListed;
-			if ($fileListed =~ m/([\S]+)/) { #todo more sanity
-				$fileListed = $1;
-				#say $fileListed;
-				if ($fileListed =~ m/.txt$/) {
-					if (!-e $fileListed) {
-						$filesNeeded = $filesNeeded . ' ' . $fileListed;
-						if (length($filesNeeded) > 10000) {
-							#say `tar -vzxf $file $filesNeeded`;
-							#say `cat $filesNeeded`;
-							$filesNeededCount++;
-							#$filesNeeded = '';
-						}
-					}
-				}
-			}
-		}
-		#say `tar -vzxf $file $filesNeeded`;
-	} else {
-		#todo warning
-	}
-}
-
-print 'Files found: ' . $filesNeededCount . "\n";
-
-sleep 1;
-
-#todo cd `html/txt`;
-
-if (!$filesNeededCount) {
-	exit;
-}
+# my $filesNeededCount = 0;
+#
+# print "Counting files...\n";
+#
+# for my $file (@files) {
+# 	chomp $file;
+# 	if ($file =~ m/([\S]+)/) { #todo more sanity
+# 		$file = $1;
+# 		my @filesListed = `tar --list -f $file`;
+# 		my $filesNeeded = '';
+# 		for my $fileListed (@filesListed) {
+# 			chomp $fileListed;
+# 			if ($fileListed =~ m/([\S]+)/) { #todo more sanity
+# 				$fileListed = $1;
+# 				#say $fileListed;
+# 				if ($fileListed =~ m/.txt$/) {
+# 					if (!-e $fileListed) {
+# 						$filesNeeded = $filesNeeded . ' ' . $fileListed;
+# 						if (length($filesNeeded) > 10000) {
+# 							#say `tar -vzxf $file $filesNeeded`;
+# 							#say `cat $filesNeeded`;
+# 							$filesNeededCount++;
+# 							#$filesNeeded = '';
+# 						}
+# 					}
+# 				}
+# 			}
+# 		}
+# 		#say `tar -vzxf $file $filesNeeded`;
+# 	} else {
+# 		#todo warning
+# 	}
+# }
+#
+# # print 'Files found: ' . $filesNeededCount . "\n";
+#
+# sleep 1;
+#
+# #todo cd `html/txt`;
+#
+# if (!$filesNeededCount) {
+# 	exit;
+# }
 
 my $filesDone = 0;
 
@@ -73,34 +73,29 @@ for my $file (@files) {
 						$filesDone++;
 						$filesNeeded = $filesNeeded . ' ' . $fileListed;
 						if (length($filesNeeded) > 10000) {
-							`tar -vzxf $file $filesNeeded`;
+							say `tar -vzxf $file $filesNeeded`;
+							say `mv -v $filesNeeded html/txt`; #todo unhardcode
 							$filesNeeded = '';
 							print ".";
-							sleep 1;
 						}
 					}
-					#say $fileListed;
-#					my $fileTarSaid = `tar -vzxf $file $fileListed`;
-#					chomp $fileTarSaid;
-#					if ($fileTarSaid) {
-#						if ($fileTarSaid eq $fileListed) {
-#							say 'done';
-#						} else {
-#							say 'hey';
-#						}
-#					}
 				}
 			}
 		}
-							say `tar -vzxf $file $filesNeeded`;
-							$filesNeeded = '';
+		if ($filesNeeded) {
+			say `tar -vzxf $file $filesNeeded`;
+			say `mv -v $filesNeeded html/txt`; #todo unhardcode
+			$filesNeeded = '';
+			print ".";
+		}
 	} else {
 		#todo warning
 	}
 }
 
-# inflate all .tar.gz files
 #
 # move all text files into html/txt
+
+print "\n";
 
 1;
