cd890cb5d44cfa6c5fc1743bbef7e229f536f9d7
Your Name <you@example.com>
2022-08-12 14:15:49 -0400

    AddressForWeb() and PhoneForWeb() basic ; add to scratch.pl

--- default/template/perl/format_message.pl
+++ default/template/perl/format_message.pl
@@ -34,6 +34,44 @@ sub SurveyForWeb { # replaces some spaces with &nbsp; to preserve text-based lay
 	return $text;
 }
 
+sub AddressForWeb {
+	my $text = shift;
+
+	if (!$text) {
+		return '';
+	}
+
+	my $urlText = $text;
+	$urlText = UriEscape($urlText);
+	my $url = "http://maps.google.com/maps/?q=$urlText";
+
+	my $formattedAddress =
+		'<a href="' .
+		$url .
+		'">' .
+		$text .
+		'</a>'
+	;
+
+	return $formattedAddress;
+	#return $urlText;
+	#return $text;
+} # AddressForWeb();
+
+sub PhoneForWeb {
+	my $text = shift;
+
+	if (!$text) {
+		return '';
+	}
+
+	my $html = '<a href="tel:' . $text . '">' . $text . '</a>';
+
+	#rough draft
+
+	return $html;
+} # PhoneForWeb()
+
 sub TextartForWeb { # replaces some spaces with &nbsp; to preserve text-based layout for html display; $text
 	my $text = shift;
 
@@ -122,51 +160,5 @@ sub FormatForRss { # replaces some spaces with &nbsp; to preserve text-based lay
 
 	return $text;
 } # FormatForRss()
-
-#sub FormatMessage { # $message, \%file
-#	my $message = shift;
-#	my %file = %{shift @_}; #todo should be better formatted
-#	#todo sanity checks
-#
-#	if ($file{'remove_token'}) {
-#		my $removeToken = $file{'remove_token'};
-#		$message =~ s/$removeToken//g;
-#		$message = trim($message);
-#	}
-#
-#	my $isTextart = 0;
-#	my $isSurvey = 0;
-#	my $isTooLong = 0;
-#
-#	if ($file{'tags_list'}) {
-#		# if there is a list of tags, check to see if there is a 'textart' tag
-#
-#		# split the tags list into @itemTags array
-#		my @itemTags = split(',', $file{'tags_list'});
-#
-#		# loop through all the tags in @itemTags
-#		while (scalar(@itemTags)) {
-#			my $thisTag = pop @itemTags;
-#			if ($thisTag eq 'textart') {
-#				$isTextart = 1; # set isTextart to 1 if 'textart' tag is present
-#			}
-#			if ($thisTag eq 'survey') {
-#				$isSurvey = 1; # set $isSurvey to 1 if 'survey' tag is present
-#			}
-#		}
-#	}
-#
-#	if ($isTextart) {
-#		# if textart, format with extra spacing to preserve character arrangement
-#		#$message = TextartForWeb($message);
-#		$message = TextartForWeb(GetFile($file{'file_path'}));
-#	} else {
-#		# if not textart, just escape html characters
-#		WriteLog('FormatMessage: calling FormatForWeb');
-#		$message = FormatForWeb($message);
-#	}
-#
-#	return $message;
-#} # FormatMessage()
 
 1;
--- doc/scratch/scratch.pl
+++ doc/scratch/scratch.pl
@@ -1,3 +1,51 @@
+
+#sub FormatMessage { # $message, \%file
+#	my $message = shift;
+#	my %file = %{shift @_}; #todo should be better formatted
+#	#todo sanity checks
+#
+#	if ($file{'remove_token'}) {
+#		my $removeToken = $file{'remove_token'};
+#		$message =~ s/$removeToken//g;
+#		$message = trim($message);
+#	}
+#
+#	my $isTextart = 0;
+#	my $isSurvey = 0;
+#	my $isTooLong = 0;
+#
+#	if ($file{'tags_list'}) {
+#		# if there is a list of tags, check to see if there is a 'textart' tag
+#
+#		# split the tags list into @itemTags array
+#		my @itemTags = split(',', $file{'tags_list'});
+#
+#		# loop through all the tags in @itemTags
+#		while (scalar(@itemTags)) {
+#			my $thisTag = pop @itemTags;
+#			if ($thisTag eq 'textart') {
+#				$isTextart = 1; # set isTextart to 1 if 'textart' tag is present
+#			}
+#			if ($thisTag eq 'survey') {
+#				$isSurvey = 1; # set $isSurvey to 1 if 'survey' tag is present
+#			}
+#		}
+#	}
+#
+#	if ($isTextart) {
+#		# if textart, format with extra spacing to preserve character arrangement
+#		#$message = TextartForWeb($message);
+#		$message = TextartForWeb(GetFile($file{'file_path'}));
+#	} else {
+#		# if not textart, just escape html characters
+#		WriteLog('FormatMessage: calling FormatForWeb');
+#		$message = FormatForWeb($message);
+#	}
+#
+#	return $message;
+#} # FormatMessage()
+
+
 							if (GetConfig('setting/admin/index/multiple_parent_means_no_parent') && scalar(@itemParents) > 1) {
 
 
