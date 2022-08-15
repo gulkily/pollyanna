53954c647ac0d8194fe0401d92db663088291247
Your Name <you@example.com>
2022-08-15 10:47:48 -0700

    add welcome.pl for carlos theme

---
+++ default/theme/carlos/template/perl/page/welcome.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -T
+
+use strict;
+use warnings;
+use 5.010;
+
+sub GetWelcomePage {
+	require_once('item_list_as_gallery.pl');
+	my %params;
+	$params{'where_clause'} = "WHERE tags_list like '%image%'";
+	my @items = DBGetItemList(\%params);
+
+	my $html = 
+		GetPageHeader('welcome') .
+		GetItemListAsGallery(\@items) .
+		GetPageFooter('welcome')
+	;
+
+	return $html;
+}
+
+1;
