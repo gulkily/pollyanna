af8301cf2ae035cb917e30be873125600ae7e284
Your Name <you@example.com>
2022-08-21 13:58:47 -0400

    add some settings for carlos, phoenix, and banana themes

---
+++ default/theme/carlos/template/sh/config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo 0 > config/setting/html/menu_top
+echo 0 > config/setting/html/menu_bottom
+echo 0 > config/setting/admin/js/loading
+echo 0 > config/setting/html/css_theme_concat
---
+++ default/theme/phoenix/template/sh/config.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo 1 > config/setting/html/menu_top
+echo 0 > config/setting/html/menu_bottom
+echo 1 > config/setting/admin/js/loading
+echo 1 > config/setting/html/css_theme_concat
