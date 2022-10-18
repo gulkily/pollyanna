41dc24fdf5876a2f77e97a4f48c5d6dd57f78ddb
Your Name <you@example.com>
2022-10-18 05:54:42 -0400

    improve help page; create separate module for help page (page.pl); add views dialog on help page; files: help.pl help_views.template widget.pl index_text_file.pl help.template

---
+++ default/template/perl/page/help.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -T
+
+use strict;
+use warnings;
+use 5.010;
+
+sub GetHelpPage { # returns html for help page (/help.html)
+	my $txtIndex = "";
+
+	$txtIndex =
+		GetPageHeader('help') .
+		GetTemplate('html/maincontent.template') .
+		GetWindowTemplate(GetTemplate('html/page/help.template'), 'Help') .
+		GetWindowTemplate(GetTemplate('html/page/help_diagnostics.template'), 'Toys') .
+		GetWindowTemplate(GetTemplate('html/page/help_views.template'), 'Views') .
+		GetStatsTable() .
+        GetPageFooter('help')
+	;
+
+	if (GetConfig('admin/js/enable')) {
+		$txtIndex = InjectJs($txtIndex, qw(settings avatar profile timestamp pingback utils clock));
+	}
+
+	return $txtIndex;
+} # GetHelpPage()
+
+1;
