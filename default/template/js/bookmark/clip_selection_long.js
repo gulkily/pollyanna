081ab1232b3bcf8263db441d836557793628ae64
Your Name <you@example.com>
2022-12-03 03:44:24 -0500

    update clip_selection_long.js

--- default/template/js/bookmark/clip_selection_long.js
+++ default/template/js/bookmark/clip_selection_long.js
@@ -1,7 +1,7 @@
-// #todo
+// #todo this is a draft which may or may not work yet
 
 window.f = document.createElement('form');
-f.setAttribute('action', 'http://www.shitmyself.com/post.html');
+f.setAttribute('action', 'http://localhost:2784/post.html');
 f.setAttribute('target', '_blank');
 f.setAttribute('method', 'POST');
 window.c = document.createElement('input');
@@ -12,7 +12,4 @@ document.body.appendChild(f);
 f.appendChild(c);
 f.submit();
 
-
-
-
-javascript:void(window.f = document.createElement('form'); f.setAttribute('action', 'http://www.shitmyself.com/post.html'); f.setAttribute('target', '_blank'); f.setAttribute('method', 'POST'); window.c = document.createElement('input'); c.setAttribute('name', 'comment'); c.setAttribute('type', 'hidden'); c.setAttribute('value', window.getSelection() + '\n' + document.title + '\n' + document.location); document.body.appendChild(f); f.appendChild(c); f.submit())
+javascript:void(window.f = document.createElement('form'); f.setAttribute('action', 'http://localhost:2784/post.html'); f.setAttribute('target', '_blank'); f.setAttribute('method', 'POST'); window.c = document.createElement('input'); c.setAttribute('name', 'comment'); c.setAttribute('type', 'hidden'); c.setAttribute('value', window.getSelection() + '\n' + document.title + '\n' + document.location); document.body.appendChild(f); f.appendChild(c); f.submit())
