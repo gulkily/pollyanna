aabad64e6edecd712e060100e5dd9dceed685c39
Your Name <you@example.com>
2022-12-04 03:41:15 -0500

    improve thanks page

--- default/theme/thankyou/template/perl/page/thanks.pl
+++ default/theme/thankyou/template/perl/page/thanks.pl
@@ -7,7 +7,9 @@ use 5.010;
 sub GetThanksPage {
 	my $html =
 		GetPageHeader('thanks') .
-		GetWindowTemplate(GetTemplate('html/page/thanks.template'), 'Thanks') .
+		GetWindowTemplate(GetTemplate('html/page/god.template'), 'Creator') .
+		GetWindowTemplate(GetTemplate('html/page/supporters.template'), 'Supporters') .
+		GetWindowTemplate(GetTemplate('html/page/enablers.template'), 'Enablers') .
 		GetPageFooter('thanks')
 	;
 
