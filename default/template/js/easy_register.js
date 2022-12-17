f3948af948804853f2582dd8b30d104d207c4ea8
Your Name <you@example.com>
2022-12-17 02:45:03 -0500

    add easy_register.js for easyreg theme

---
+++ default/template/js/easy_register.js
@@ -0,0 +1,11 @@
+<!-- easy_register.js -->
+
+function EasyRegister (t) {
+	MakeKey(t);
+	SetPrefs('sign_by_default', 1);
+	PubKeyPing();
+	doSolvePuzzle(t);
+	return false;
+}
+
+<!-- easy_register.js -->
