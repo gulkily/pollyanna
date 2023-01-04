c231ae0ef2a3dae93a728ed61dac281bed2aad36
Your Name <you@example.com>
2023-01-04 18:32:46 -0500

    add to yavista theme config.sh

--- default/theme/yavista/template/sh/config.sh
+++ default/theme/yavista/template/sh/config.sh
@@ -4,6 +4,8 @@ echo 0 > config/setting/html/item_page/toolbox_publish
 echo 0 > config/setting/html/item_page/toolbox_search
 echo 1 > config/setting/html/mourn
 echo 1 > config/setting/html/monochrome
-echo 1 > config/setting/admin/js/openpgp_keygen_prompt_for_usernamw
+echo 1 > config/setting/admin/js/openpgp_keygen_prompt_for_username
+echo 1 > config/setting/admin/js/loading
+echo "Pray..." > config/string/en/meditate
 echo "#fff0ef" > config/setting/html/color/background
 echo "#403000" > config/setting/html/color/text
