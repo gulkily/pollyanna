cfc12c9737be5cc83f00befa62f6c90bb3389ab8
Your Name <you@example.com>
2023-01-04 15:52:43 -0500

    add username prompt setting to yavista config.sh

--- default/theme/yavista/template/sh/config.sh
+++ default/theme/yavista/template/sh/config.sh
@@ -4,5 +4,6 @@ echo 0 > config/setting/html/item_page/toolbox_publish
 echo 0 > config/setting/html/item_page/toolbox_search
 echo 1 > config/setting/html/mourn
 echo 1 > config/setting/html/monochrome
+echo 1 > config/setting/admin/js/openpgp_keygen_prompt_for_usernamw
 echo "#fff0ef" > config/setting/html/color/background
 echo "#403000" > config/setting/html/color/text
