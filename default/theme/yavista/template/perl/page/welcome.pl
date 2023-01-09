e68c1c2035d22d67885140043d59c6cc26d4053c
Your Name <you@example.com>
2023-01-09 14:33:27 -0500

    improve welcome page on yavista theme

--- default/theme/yavista/template/perl/page/welcome.pl
+++ default/theme/yavista/template/perl/page/welcome.pl
@@ -7,17 +7,17 @@ use 5.010;
 sub GetWelcomePage {
 	my $html =
 		GetPageHeader('welcome') .
-			GetWindowTemplate(GetTemplate('html/page/welcome.template'), '') . # this is a bit of a hack, should really not be a titlebar
-			#GetWindowTemplate(GetTemplate('html/page/content.template'), 'Please Share') .
-			#GetWindowTemplate(GetTemplate('html/page/rules.template'), 'Ground Rules') .
-			#GetWindowTemplate(GetTemplate('html/page/privacy.template'), 'Privacy') .
-			#GetWindowTemplate(GetTemplate('html/form/enter.template'), 'Create Profile') .
-			#GetWindowTemplate(GetTemplate('html/form/guest.template'), 'Guest') .
-			#GetWindowTemplate(GetTemplate('html/form/emergency.template'), 'Emergency Contact Form') .
-			'<form action="/post.html" method=GET id=compose class=submit name=compose target=_top>' .
-			GetWriteForm('Prayer/Thanks') .
-			'</form>' . #todo unhack this
-			GetPageFooter('welcome')
+		GetWindowTemplate(GetTemplate('html/page/welcome.template'), '') . # this is a bit of a hack, should really not be a titlebar
+		#GetWindowTemplate(GetTemplate('html/page/content.template'), 'Please Share') .
+		#GetWindowTemplate(GetTemplate('html/page/rules.template'), 'Ground Rules') .
+		#GetWindowTemplate(GetTemplate('html/page/privacy.template'), 'Privacy') .
+		#GetWindowTemplate(GetTemplate('html/form/emergency.template'), 'Emergency Contact Form') .
+		'<form action="/post.html" method=GET id=compose class=submit name=compose target=_top>' .
+		GetWriteForm('Prayer/Thanks') .
+		'</form>' . #todo unhack this
+		GetWindowTemplate(GetTemplate('html/form/guest.template'), 'Guest') .
+		GetWindowTemplate(GetTemplate('html/form/enter.template'), 'Create Profile') .
+		GetPageFooter('welcome')
 	;
 
 	if (GetConfig('admin/js/enable')) {
