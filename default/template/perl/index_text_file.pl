6e50b5fde67d4b67b7772f4fcee8b8ee363b2e4d
Your Name <you@example.com>
2023-01-18 02:52:53 -0500

    add self_admin_minimum_score setting

--- default/template/perl/index_text_file.pl
+++ default/template/perl/index_text_file.pl
@@ -772,14 +772,16 @@ sub IndexTextFile { # $file | 'flush' ; indexes one text file into database
 										elsif (
 											$hashTag eq 'admin' &&
 											GetConfig('admin/allow_self_admin_when_adminless') &&
-											!DBGetAdminCount()
+											!DBGetAdminCount() &&
+											DBGetAuthorScore($authorKey) >= GetConfig('setting/admin/self_admin_minimum_score')
 										) {
 											$approveStatus = 2;
 											$approveReason = 'self-admin when adminless is allowed';
 										}
 										elsif (
 											$hashTag eq 'admin' &&
-											GetConfig('admin/allow_self_admin_whenever')
+											GetConfig('admin/allow_self_admin_whenever') &&
+											DBGetAuthorScore($authorKey) >= GetConfig('setting/admin/self_admin_minimum_score')
 										) {
 											$approveStatus = 3;
 											$approveReason = 'self-admin whenever is allowed';
