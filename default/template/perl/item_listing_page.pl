b0d5d31cb619e6502b7eb81802d454df7121a847
Your Name <you@example.com>
2023-01-30 07:42:36 -0500

    add search keywords to GetItemListingPage() ; item_listing_page.pl

--- default/template/perl/item_listing_page.pl
+++ default/template/perl/item_listing_page.pl
@@ -166,6 +166,7 @@ sub GetItemListingPage { # $pageQuery, $pageMode (dialog_list, full_items, dialo
 
 	if ($pageQuery =~ m/[^\s]+/) {
 		# $pageQuery does not have any spaces, so it's a page name
+		# page_info/
 		my $pageDescription = GetStringNoFallback('page_intro/' . $pageQuery);
 		if ($pageDescription) {
 			$pageDescription = str_replace("\n", "<br>\n", $pageDescription);
