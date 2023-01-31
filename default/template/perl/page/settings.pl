41a13305f9fc888669bb68f8c0426c1debd8b977
Your Name <you@example.com>
2023-01-30 23:01:20 -0500

    expose more controls to beginner layer because they're frequently used

--- default/template/perl/page/settings.pl
+++ default/template/perl/page/settings.pl
@@ -67,7 +67,8 @@ sub GetSettingsPage { # returns html for settings page (/settings.html)
 	$txtIndex .= GetOperatorDialog();
 
 	if (GetConfig('admin/js/enable') && GetConfig('admin/js/dragging')) {
-		$txtIndex .= '<span class=advanced>' . GetDialogX(GetTemplate('html/form/annoyances.template'), 'Annoyances') . '</span>';
+		# $txtIndex .= '<span class=advanced>' . GetDialogX(GetTemplate('html/form/annoyances.template'), 'Annoyances') . '</span>';
+		$txtIndex .= GetDialogX(GetTemplate('html/form/annoyances.template'), 'Annoyances');
 	}
 	#$txtIndex .= GetMenuTemplate();
 
