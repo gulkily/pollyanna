4802ebe1213d3d7331cc8c9b93a6140f952091ad
Your Name <you@example.com>
2023-01-31 00:59:57 -0500

    move puzzle, settings, and tos dialogs to separate modules and clean up dialog.pl

--- default/template/perl/dialog.pl
+++ default/template/perl/dialog.pl
@@ -7,46 +7,9 @@ use 5.010;
 require_once('dialog/chain_log.pl');
 require_once('dialog/access.pl');
 require_once('get_dialog.pl');
-
-sub GetTosDialog {
-	my $tosText = GetString('tos');
-	#$tosText = str_replace("\n", '<br>', $tosText); #todo improve this
-
-	$tosText = '<p class=txt>' . $tosText . '</p>';
-	#$tosText .= '<p><a href="/post.html?comment=tos">[Do You Agree?]</a></p>';
-	my $tosWindow = GetDialogX(
-		$tosText,
-		'Terms of Service',
-	);
-	#	my @tosItems
-	return $tosWindow;
-}
-
+require_once('dialog/tos.pl');
 require_once('dialog/write.pl');
-
-sub GetPuzzleDialog { # returns write form (for composing text message)
-	return 'GetPuzzleDialog() is not finished'; #draft
-	my $puzzleForm = GetDialogX(GetTemplate('html/form/puzzle.template'), 'Puzzle?');
-	#dirty hack
-	#$writeForm =~ s/textarea/input type=hidden/g;
-	WriteLog('GetPuzzleDialog()');
-
-	if (GetConfig('admin/php/enable')) {
-		if (GetConfig('admin/php/enable') && !GetConfig('admin/php/rewrite')) {
-			# if php is enabled but rewrite is disabled
-			# change submit target to post.php
-			my $postHtml = 'post\\.html'; # post.html
-			$puzzleForm =~ s/$postHtml/post.php/; #todo is this necessary?
-		}
-	}
-
-	my $initText = '';
-	$puzzleForm =~ s/\$initText/$initText/g;
-
-	$puzzleForm = '<form action="/post.html" method=GET id=compose class=submit name=compose target=_top>' . $puzzleForm . '</form>';
-
-	return $puzzleForm;
-} # GetPuzzleDialog ()
+require_once('dialog/puzzle.pl');
 
 sub GetSearchDialog { # search dialog for search page
 	my $searchForm = GetTemplate('html/form/search.template');
@@ -80,13 +43,11 @@ sub GetSimpleDialog { # dialogType ; gets simple dialog based on template/html/p
 } # GetSimpleDialog()
 
 sub GetOperatorDialog {
-
 	my $operatorTemplate = GetTemplate('html/form/operator.template');
 	my $operatorWindow = GetDialogX($operatorTemplate, 'Operator');
 	$operatorWindow = '<form id=frmOperator name=frmOperator class=admin>' . $operatorWindow . '</form>';
 	return $operatorWindow;
-	#return 'hi';
-}
+} # GetOperatorDialog()
 
 sub GetAnnoyancesDialog { # returns settings dialog
 # sub GetAnnoyancesWindow {
@@ -94,38 +55,6 @@ sub GetAnnoyancesDialog { # returns settings dialog
 	return '<form>' . GetDialogX(GetTemplate('html/form/annoyances.template'), 'Annoyances') . '</form>';
 } # GetAnnoyancesDialog()
 
-sub GetSettingsDialog { # returns settings dialog
-# sub GetSettingsWindow {
-	WriteLog('GetSettingsDialog() BEGIN');
-
-	my $settingsTemplate = GetTemplate('html/form/settings.template');
-
-	if (GetConfig('admin/js/dragging')) {
-	} else {
-		# kind of a hack
-		# dragging is disabled, so make the option unavailable
-		$settingsTemplate = AddAttributeToTag($settingsTemplate, 'input id=chkDraggable', 'disabled', '');
-		$settingsTemplate = str_replace('Allow dialog reposition', '<span disabled>Allow dialog reposition (N/A)</span>', $settingsTemplate);
-		#$settingsTemplate = str_replace('Enable draggable interface<noscript><b>*</b></noscript>', '<span </span>', $settingsTemplate);
-
-		#		$settingsTemplate = str_replace(
-		#			'<input type=checkbox id=chkDraggable name=chkDraggable onchange="if (window.SaveCheckbox) { SaveCheckbox(this, \'draggable\'); }">',
-		#			'<input type=checkbox id=chkDraggable name=chkDraggable disabled>',
-		#			$settingsTemplate
-		#		);
-	}
-
-	my $settingsWindow = GetDialogX($settingsTemplate, 'Settings');
-	$settingsWindow =
-		'<form action="/settings.html" id=frmSettings name=frmSettings>' .
-			GetDialogX($settingsTemplate, 'Settings') .
-			'</form>'
-	;
-	#todo template this, and fill in the current page url
-
-	WriteLog('GetSettingsDialog: return $settingsWindow = ' . length($settingsWindow) . ' bytes');
-
-	return $settingsWindow;
-} # GetSettingsDialog()
+require_once('dialog/settings.pl');
 
 1;
