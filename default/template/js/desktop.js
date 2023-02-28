ebf0813a30506fc6220fca034c338324c5de1aab
Your Name <you@example.com>
2023-02-28 04:28:49 -0500

    only activate annoying mode on desktop page (not on the welcome page)

--- default/template/js/desktop.js
+++ default/template/js/desktop.js
@@ -1,17 +1,19 @@
 // desktop.js
 function DesktopOnLoad () {
 	//alert('DEBUG: DesktopOnLoad()');
-	SetPrefs('draggable', 1);
-	SetPrefs('draggable_activate', 1);
-	SetPrefs('draggable_spawn', 1);
-	SetPrefs('draggable_spawn_focus', 1);
-	SetPrefs('draggable', 1);
-	SetPrefs('draggable_restore', 1);
-	SetPrefs('draggable_restore_collapsed', 1);
-	SetActiveDialog(GetParentDialog(this));
-	DraggingInit(0);
-	DraggingMakeFit(0);
-	DraggingInit(0);
-	displayNotification('Annoying mode activated. I hope you have a good mouse. Click Reset to make it stop.');
+	if (window.location.href.indexOf('desktop') != -1) {
+		SetPrefs('draggable', 1);
+		SetPrefs('draggable_activate', 1);
+		SetPrefs('draggable_spawn', 1);
+		SetPrefs('draggable_spawn_focus', 1);
+		SetPrefs('draggable', 1);
+		SetPrefs('draggable_restore', 1);
+		SetPrefs('draggable_restore_collapsed', 1);
+		SetActiveDialog(GetParentDialog(this));
+		DraggingInit(0);
+		DraggingMakeFit(0);
+		DraggingInit(0);
+		displayNotification('Annoying mode activated. Mouse is recommended. Use Reset button to stop.');
+	}
 } // DesktopOnLoad()
 // / desktop.js
