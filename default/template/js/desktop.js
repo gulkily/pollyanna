f32b0e3443b3d410f6d2063779fe346404b8170c
Your Name <you@example.com>
2023-03-02 11:28:06 -0500

    on desktop page, do not reactivate dragging mode if it is already activated ; desktop.js

--- default/template/js/desktop.js
+++ default/template/js/desktop.js
@@ -3,18 +3,20 @@ function DesktopOnLoad () {
 	//alert('DEBUG: DesktopOnLoad()');
 	if (window.location.href.indexOf('desktop') != -1) {
 		if (window.DraggingInit && window.DraggingMakeFit && window.SetActiveDialog && window.GetParentDialog && window.displayNotification) {
-			SetPrefs('draggable', 1);
-			SetPrefs('draggable_activate', 1);
-			SetPrefs('draggable_spawn', 1);
-			SetPrefs('draggable_spawn_focus', 1);
-			SetPrefs('draggable', 1);
-			SetPrefs('draggable_restore', 1);
-			SetPrefs('draggable_restore_collapsed', 1);
-			SetActiveDialog(GetParentDialog(this));
-			DraggingInit(0);
-			DraggingMakeFit(0);
-			DraggingInit(0);
-			displayNotification('Annoying mode activated. Mouse is recommended. Use Reset button to stop.');
+			if (! GetPrefs('draggable')) {
+				SetPrefs('draggable', 1);
+				SetPrefs('draggable_activate', 1);
+				SetPrefs('draggable_spawn', 1);
+				SetPrefs('draggable_spawn_focus', 1);
+				SetPrefs('draggable', 1);
+				SetPrefs('draggable_restore', 1);
+				SetPrefs('draggable_restore_collapsed', 1);
+				SetActiveDialog(GetParentDialog(this));
+				DraggingInit(0);
+				DraggingMakeFit(0);
+				DraggingInit(0);
+				displayNotification('Annoying mode activated.');
+			}
 		}
 	}
 } // DesktopOnLoad()
