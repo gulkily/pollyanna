53a837310390479bb8d5929b0279786aedb5c35e
Ilya Gulko <thankyou@mit.edu>
2023-03-18 05:18:56 -0400

    ilyag theme: add javascript to welcome page

--- default/theme/ilyag/template/perl/page/welcome.pl
+++ default/theme/ilyag/template/perl/page/welcome.pl
@@ -9,11 +9,22 @@ sub GetWelcomePage {
 	my %params;
 	$params{'where_clause'} = "WHERE tags_list LIKE '%approve%' AND score > 0";
 	my @files = DBGetItemList(\%params);
-	return
+
+	my $welcomePage =
 		GetPageHeader('welcome') .
 		GetItemListHtml(\@files) .
 		GetPageFooter('welcome')
 	;
+
+	if (GetConfig('admin/js/enable')) {
+		my @js = qw(avatar puzzle settings profile utils timestamp clock fresh table_sort voting write);
+		if (GetConfig('admin/php/enable')) {
+			push @js, 'write_php'; # write.html
+		}
+		$welcomePage = InjectJs($welcomePage, @js)
+	}
+
+	return $welcomePage
 }
 
 1
