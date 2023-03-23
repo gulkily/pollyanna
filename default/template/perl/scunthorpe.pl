9e32e2f740b071f2902d3be11bd459146daa31ee
Ilya Gulko <thankyou@mit.edu>
2023-03-23 11:31:12 -0400

    improve smell in scunthorpe.pl

--- default/template/perl/scunthorpe.pl
+++ default/template/perl/scunthorpe.pl
@@ -5,12 +5,16 @@ use warnings;
 use 5.010;
 use utf8;
 
-sub IndexScunthorpe {
+sub IndexScunthorpe { # $textRef ; adds to vote table, returns same hash plus index log
+# $textRef is a reference to a hash of {'message', 'messageLog', 'fileHash'}
 	my $cussWords = GetTemplate('list/scunthorpe');
 	# #scunthorpe is a very basic word filter
 	# it can be used to apply a negative score to
 	# common inflammatory or offensive terms
 	# stored \n-separated in config/list/scunthorpe
+	#
+	# if any words are found, a vote of "scunthorpe" will be added to the item
+	# returns hash reference to hash, message, and parse log
 
 	my $textRef = shift;
 	my %text = %{$textRef};
@@ -19,7 +23,7 @@ sub IndexScunthorpe {
 	my @indexMessageLog = @{$text{'messageLog'}};
 	my $fileHash = $text{'fileHash'};
 
-	WriteLog('IndexScunthorpe: $fileHash = ' . $fileHash);
+	WriteLog('IndexScunthorpe($fileHash = ' . $fileHash . '); caller = ' . join(',', caller));
 
 	if ($cussWords) {
 		WriteLog('IndexScunthorpe: $cussWords is TRUE');
@@ -43,6 +47,9 @@ sub IndexScunthorpe {
 						$message =~ m/$word$/i
 					)
 				) {
+					# match word on beginning of line, end of line, or between two whitespaces
+					# this helps reduce the scunthorpe problem
+
 					WriteLog('IndexScunthorpe: scunthorpe: $word = ' . $word);
 					$cussWordCount++;
 				}
@@ -55,7 +62,10 @@ sub IndexScunthorpe {
 				DBAddVoteRecord($fileHash, 0, 'scunthorpe');
 			}
 		} # @cussWord
-	} # $cussWords
+	} # if ($cussWords)
+	else {
+		WriteLog('IndexScunthorpe: warning: $cussWords was false, skipping');
+	}
 
 	$text{'message'} = $message;
 	$text{'messageLog'} = \@indexMessageLog;
