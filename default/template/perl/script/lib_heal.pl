b254e3232888f8aa2b3f1fcb3a2bbfca407c1eef
Ilya Gulko <thankyou@mit.edu>
2023-04-01 13:03:52 -0400

    add comments to flesh out lib_heal.pl

--- default/template/perl/script/lib_heal.pl
+++ default/template/perl/script/lib_heal.pl
@@ -5,7 +5,22 @@
 $result = system("perl -e 'use lib qw(lib); use URI::Encode;'");
 if ($result) {
     # problem including URI::Encode
-    system("mkdir lib; mkdir lib/URI; cp default/template/perl/lib/URI/Encode.pm lib/URI/Encode.pm");
+
+    # there are three possible solutions to this problem:
+
+    # 1) we can try to use regular CPAN to install the library
+    # challenge: for this, it is easier if we use sudo
+
+    # 2) we can try to fill the library in from default (copy we brought along with us in the repo)
+    # challenge: it may be the wrong version
+
+    # 3) we may just need to remove the library from lib/ if a version conflict is causing the problem
+    # challenge: how to detect this?
+
+    { #2
+        # if the library is missing, we can try to fill it in from the one we brought with us in the repo:
+        system("mkdir lib; mkdir lib/URI; cp default/template/perl/lib/URI/Encode.pm lib/URI/Encode.pm");
+    }
 } else {
     # 0 means it is ok
 }
