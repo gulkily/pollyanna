42f08c7679da862c847fa30ea440718dc412c8e1
Ilya Gulko <thankyou@mit.edu>
2023-04-28 14:35:02 -0400

    improve hypercode theme reset config script

--- default/theme/hypercode/template/sh/reset_config.sh
+++ default/theme/hypercode/template/sh/reset_config.sh
@@ -7,9 +7,14 @@ echo 0 > config/setting/admin/image/enable
 echo 0 > config/setting/admin/js/loading
 echo 0 > config/setting/admin/js/dragging
 echo 0 > config/setting/admin/js/enable
+echo 0 > config/setting/admin/js/debug
+echo 1 > config/setting/html/reset_button
 echo 0 > config/setting/html/item_page/toolbox_search
+echo 0 > config/setting/html/menu_layer_controls
 echo 0 > config/setting/html/write_options
-echo 0 > config/setting/php/route_notify_printed_time
+echo 0 > config/setting/admin/php/cookie_inbox
+echo 0 > config/setting/admin/php/route_notify_printed_time
+echo 1 > config/setting/html/monochrome
 #echo 0 > config/setting/admin/index/create_system_tags
 echo "hypercode chicago" > config/setting/theme
 bash hike.sh frontend
