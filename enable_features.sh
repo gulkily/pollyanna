12c3f600442bc80e388f7d92785328911605ff3c
Ilya Gulko <thankyou@mit.edu>
2023-05-08 04:30:21 -0400

    improve enable_features.sh

--- enable_features.sh
+++ enable_features.sh
@@ -4,7 +4,7 @@ echo enabling the following features:
 
 echo javascript
 echo draggable interface
-echo openpgp.js
+echo openpgp.js and keychain
 echo loading indicator
 echo php connectors
 echo uploading of files
@@ -16,6 +16,7 @@ echo 1 > config/setting/admin/php/enable
 echo 1 > config/setting/admin/js/dragging
 echo 1 > config/setting/admin/js/loading
 echo 1 > config/setting/admin/js/openpgp
+echo 1 > config/setting/admin/js/openpgp_keychain
 echo 1 > config/setting/admin/upload/enable
 echo 1 > config/setting/admin/allow_admin_permissions_tag_lookup
 echo 1 > config/setting/admin/allow_self_admin_when_adminless
