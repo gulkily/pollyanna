7dc8eb9d3b1573755aec2f5c5e2af0ea10082f02
Ilya Gulko <thankyou@mit.edu>
2023-05-17 12:10:06 -0400

    improve lib_heal.pl

--- doc/scratch/lib_heal.pl
+++ doc/scratch/lib_heal.pl
@@ -4,25 +4,41 @@
 
 $result = system("perl -e 'use lib qw(lib); use URI::Encode;'");
 if ($result) {
-    # problem including URI::Encode
+	# problem including URI::Encode
 
-    # there are three possible solutions to this problem:
+	# there are three possible solutions to this problem:
 
-    # 1) we can try to use regular CPAN to install the library
-    # challenge: for this, it is easier if we use sudo
+	# 1) we can try to use regular CPAN to install the library
+	# challenge: for this, it is easier if we use sudo
 
-    # 2) we can try to fill the library in from default (copy we brought along with us in the repo)
-    # challenge: it may be the wrong version
+	# 2) we can try to fill the library in from default (copy we brought along with us in the repo)
+	# challenge: it may be the wrong version
 
-    # 3) we may just need to remove the library from lib/ if a version conflict is causing the problem
-    # challenge: how to detect this?
+	# 3) we may just need to remove the library from lib/ if a version conflict is causing the problem
+	# challenge: how to detect this?
 
-    { #2
-        # if the library is missing, we can try to fill it in from the one we brought with us in the repo:
-        system("mkdir lib; mkdir lib/URI; cp default/template/perl/lib/URI/Encode.pm lib/URI/Encode.pm");
-    }
+	my $os = '';
+	if () {
+	    $os = 'windows';
+	} elsif () {
+	    $os = 'gnu';
+	} elsif () {
+	    $os = 'mac';
+    } elsif () {
+        $os = 'solaris';
+    } elsif () {
+        $os = 'freebsd';
+	} else {
+	    $os = 'unknown';
+	}
+	if ($os eq 'gnu' || $os eq 'mac' || $os eq 'freebsd' || $os eq 'solaris') { # 2)
+		# if the library is missing, we can try to fill it in from the one we brought with us in the repo:
+		system("mkdir lib; mkdir lib/URI; cp default/template/perl/lib/URI/Encode.pm lib/URI/Encode.pm");
+	} elsif ($os eq 'windows') {
+	    system("mkdir lib; mkdir lib/URI; copy default\\template\\perl\\lib\\URI\\Encode.pm lib\\URI\\Encode.pm");
+	}
 } else {
-    # 0 means it is ok
+	# 0 means it is ok
 }
 
 #$result = system("perl -e 'use lib qw(lib); use URI::Escape;'");
