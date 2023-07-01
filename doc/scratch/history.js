4ac608861abd406473fdb3bc5a731a22dbbf5960
Your Name <you@example.com>
2023-07-01 17:20:55 -0400

    improve comments in and remove GT character from history.js

--- doc/scratch/history.js
+++ doc/scratch/history.js
@@ -12,7 +12,7 @@
 //				var dialogId = GetDialogId(allOpenDialogs[iDialog]);
 //				var gt = unescape('%3E');
 //
-//				if (dialogTitle.length > 24) {
+//				if (24 < dialogTitle.length) {
 //					dialogTitle = dialogTitle.substr(0, 24);
 //				}
 //
@@ -38,6 +38,6 @@
 //			}
 //		}
 //	}
-//} // UpdateDialogList()
+//} // UpdateHistoryList()
 
 // / history.js
