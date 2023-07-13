edf62e2951b9cd28e1d373f07534e6a2826faca9
Your Name <you@example.com>
2023-07-13 00:13:55 -0400

    add GetPersonPage() skeleton ; get_person_page.pl

---
+++ default/template/perl/get_person_page.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -T
+
+use strict;
+use warnings;
+use 5.010;
+
+sub GetPersonPage { # $personName
+	my $personName = shift;
+
+	if ($personName =~ m/^([a-zA-Z0-9]+)$/) { # #todo use a validator function instead of regex
+		$personName = $1;
+		WriteLog('GetPersonPage: sanity check passed, $personName = ' . $personName);
+	} else {
+		WriteLog('GetPersonPage: warning: sanity check failed on $personName; caller = ' . join(',', caller));
+		return '';
+	}
+
+	#todo add person.template
+
+	my %params;
+	$params{'where_clause'} = "WHERE file_hash IN (SELECT file_hash FROM item_flat WHERE author_key IN(SELECT author_key FROM author_flat WHERE author_alias = '$personName'))";
+	my @files = DBGetItemList(\%params);
+
+	my $itemList = GetItemListHtml(\@files);
+
+	my $html =
+		GetPageHeader('person') .
+		$itemList .
+		GetPageFooter('person')
+	;
+
+	return $html;
+} # GetPersonPage()
+
+1;
