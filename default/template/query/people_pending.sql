53ded6dcf52890758057961cb029811414a3b8e4
Your Name <you@example.com>
2023-07-17 20:03:52 -0400

    move "pending approval" query to a template ; people.pl

---
+++ default/template/query/people_pending.sql
@@ -0,0 +1,11 @@
+SELECT
+	file_hash,
+	author_key AS author_id,
+	'' AS tagset_pending,
+	author_key
+FROM item_flat
+WHERE
+	tags_list LIKE '%,pubkey,%'
+	AND (tags_list NOT LIKE '%,approve,%' AND tags_list NOT like '%,person,%')
+	AND author_key NOT IN (SELECT author_key FROM author_flat WHERE author_alias = 'Guest')
+
