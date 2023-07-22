4e7e54329cf545ee42b75765276dbab1377d4f32
Ilya Gulko <thankyou@mit.edu>
2023-07-22 19:34:50 -0400

    improve people_guest.sql

--- default/template/query/people_guest.sql
+++ default/template/query/people_guest.sql
@@ -1,12 +1,12 @@
 SELECT
 	file_hash,
 	author_key AS author_id,
-	'' AS tagset_pending,
-	author_key
-FROM item_flat
+	author_key,
+	author_seen,
+	item_count
+FROM author_flat
 WHERE
-	tags_list LIKE '%,pubkey,%'
-	AND (tags_list NOT LIKE '%,approve,%' AND tags_list NOT like '%,person,%')
-	AND author_key IN (SELECT author_key FROM author_flat WHERE author_alias = 'Guest')
+	author_alias != 'Guest'
+	AND author_score >= 0
 ORDER BY
 	author_seen DESC
