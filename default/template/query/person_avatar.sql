62cacae1d1ddb0bc85eb366ebb86720e3b610fa8
Your Name <you@example.com>
2023-07-25 19:58:11 -0400

    fix person_avatar.sql

--- default/template/query/person_avatar.sql
+++ default/template/query/person_avatar.sql
@@ -1,8 +1,11 @@
-SELECT author_alias, c.file_hash FROM
-( SELECT * FROM item_flat ) a
-JOIN ( SELECT * FROM vote) b ON (a.file_hash = b.ballot_hash)
-JOIN (SELECT * FROM item_flat) c ON (b.ballot_hash = c.file_hash)
-JOIN author_flat ON (author_flat.author_key = a.author_key)
+SELECT
+	c.file_hash,
+	author_alias
+FROM
+	( SELECT * FROM item_flat ) a
+	JOIN ( SELECT * FROM vote) b ON (a.file_hash = b.ballot_hash)
+	JOIN (SELECT * FROM item_flat) c ON (b.file_hash = c.file_hash)
+	JOIN author_flat ON (author_flat.author_key = a.author_key)
 WHERE
 	b.vote_value = 'avatar'
 	AND author_flat.author_alias = ?
