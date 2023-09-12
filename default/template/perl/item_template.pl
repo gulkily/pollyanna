7966a3908499197c41e8c8e6d9e15dba932ac423
Your Name <you@example.com>
2023-09-12 08:33:07 -0400

    in GetItemTemplate(), when outputting an image item, link the image to the item's page ; item_template.pl

--- default/template/perl/item_template.pl
+++ default/template/perl/item_template.pl
@@ -37,7 +37,7 @@ sub GetItemTemplateBody {
 
 	if ($file{'item_type'} eq 'image') {
 		WriteLog('GetItemTemplateBody: item_type = ' . $file{'item_type'});
-		my $imageContainer = GetImageContainer($file{'file_hash'}, $file{'item_name'}, 0);
+		my $imageContainer = GetImageContainer($file{'file_hash'}, $file{'item_name'}, 1);
 		$itemTemplateBody = GetTemplate('html/item/item.template'); # GetItemTemplate()
 		$itemTemplateBody = str_replace('$itemText', $imageContainer, $itemTemplateBody); #todo #bug $itemText is always empty here
 	}
