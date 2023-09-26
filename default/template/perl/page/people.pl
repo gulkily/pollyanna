c230138e40eadd826551db462c1480b16c847866
Ilya Gulko <ilya@ilyagulko.com>
2023-09-26 07:56:28 -0400

    improve people page by changing name of Awaiting Approval dialog ; people.pl

--- default/template/perl/page/people.pl
+++ default/template/perl/page/people.pl
@@ -30,7 +30,7 @@ sub GetPeoplePage {
 	{
 		my %params;
 		$params{'no_empty'} = 1;
-		$pending = GetQueryAsDialog('people_pending', 'Pending Approval', '', \%params);
+		$pending = GetQueryAsDialog('people_pending', 'Awaiting Approval', '', \%params);
 	}
 
 	my $guests = '';
