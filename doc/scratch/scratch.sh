a026d265f48afbac149a9f4c26e39c6d8bd5d8de
Ilya Gulko <ilya@ilyagulko.com>
2023-10-06 07:44:41 -0400

    add hike.sh

---
+++ doc/scratch/scratch.sh
@@ -0,0 +1,13 @@
+#if [ $1 = sql ]
+#	then
+#		#sqlite3 cache/b/index.sqlite3 "${@:2}"
+#		#sqlite3 cache/b/index.sqlite3 "${*:2}"
+#		exit
+#fi
+
+# sanity checks for git and cut
+# check that git command exists
+# check that cut command exists
+# check that git rev-parse HEAD returns a string
+
+
