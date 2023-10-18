2d91a33677f4b3cb93e5e87b67b0c4a9ac16e3b6
Ilya Gulko <ilya@ilyagulko.com>
2023-10-18 19:05:17 -0400

    improve nocookie page

--- default/template/perl/page/nocookie.pl
+++ default/template/perl/page/nocookie.pl
@@ -4,15 +4,20 @@ use strict;
 use warnings;
 use utf8;
 
-sub GetNocookiePage {
+sub GetNoCookiePage {
 	my $html =
-		GetPageHeaderWithoutMenu('nocookie') .
+		GetPageHeader('nocookie') .
+		GetTemplate('html/maincontent.template') .
 		GetDialogX( "<p>Please forgive me, friend, <br>but you must <a href=/profile.html>register</a> first, <br>before you do that</p>", 'No Cookie Haiku' ) .
 		GetDialogX( "<p>Return to this page <br>via bookmarks, history, <br>or the Back button.</p>" , 'Tips' ) .
-		GetPageFooterWithoutMenu('nocookie')
+		GetPageFooter('nocookie')
 	;
 
+	if (GetConfig('admin/js/enable')) {
+		$html = InjectJs($html, qw(utils settings avatar profile upload paste));
+	}
+
 	return $html;
-}
+} # sub GetNoCookiePage()
 
 1;
