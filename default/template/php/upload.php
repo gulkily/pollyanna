94099555546d0049388d5462c52b8d6ad607a970
Ilya Gulko <ilya@ilyagulko.com>
2023-10-21 14:36:23 -0400

    improve and clean up upload.php

--- default/template/php/upload.php
+++ default/template/php/upload.php
@@ -124,8 +124,8 @@ if (!empty($_FILES['uploaded_file'])) {
 							WriteLog(`cd $pwd`);
 						}
 
-						WriteLog("cd $scriptDir ; ./index.pl \"html/$path\"");
-						WriteLog(`cd $scriptDir ; ./index.pl "html/$path"`);
+						WriteLog("cd $scriptDir ; perl -T config/template/perl/index.pl \"html/$path\"");
+						WriteLog(`cd $scriptDir ; perl -T config/template/perl/index.pl "html/$path"`);
 
 						if ($pwd) {
 							WriteLog("cd $pwd");
@@ -134,41 +134,19 @@ if (!empty($_FILES['uploaded_file'])) {
 
 						$hash = GetFileHash("$path");
 
+						WriteLog("GetFileHash($path) returned $hash");
+
 						WriteLog("cd $scriptDir ; ./pages.pl \"$hash\"");
 						WriteLog(`cd $scriptDir ; ./pages.pl "$hash"`);
 
-						$replyTo = '';
-						if ($_POST && isset($_POST['replyto'])) {
-							# if file was posted as reply to something, create another item to attach them together
-							# this creates an item with a >> reference and a child: token
-
-							$replyTo = $_POST['replyto'];
-							$replyTo = IsItem($replyTo);
-							if ($replyTo) {
-								$linkItem = ">>$replyTo" . "\n" . "Child: $hash";
-								$linkItemHash = sha1($linkItem);
-								$txtDir = GetDir('txt');
-								$linkItemPath = substr($linkItemHash, 0, 2) . "/" . substr($linkItemHash, 2, 2) . "$linkItemHash.txt";
-								PutFile("$txtDir/$linkItemPath", $linkItem);
-
-								if ($pwd) {
-									WriteLog("cd $pwd");
-									WriteLog(`cd $pwd`);
-								}
-
-								WriteLog("cd $scriptDir ; ./index.pl \"$txtDir/$linkItemPath\"");
-								WriteLog(`cd $scriptDir ; ./index.pl "$txtDir/$linkItemPath"`);
-
+						if (isset($replyTo) && $replyTo) {
+							WriteLog("\$replyTo = $replyTo");
+							if (IsItem($replyTo)) {
 								WriteLog("cd $scriptDir ; ./pages.pl \"$replyTo\"");
 								WriteLog(`cd $scriptDir ; ./pages.pl "$replyTo"`);
-
-								if ($pwd) {
-									WriteLog("cd $pwd");
-									WriteLog(`cd $pwd`);
-								}
-							} else {
-								WriteLog('upload.php: warning: $_POST[replyto] was passed, but $replyTo failed sanity check');
 							}
+						} else {
+							WriteLog("\$replyTo not found");
 						}
 
 						if ($pwd) {
