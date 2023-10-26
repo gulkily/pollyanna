a6ef4b9e26576a11f9b4e53db430ae491396829c
Ilya Gulko <ilya@ilyagulko.com>
2023-10-26 13:38:36 -0400

    improve debug output in ImageMakeThumbnails() ; image_thumbnail.pl

--- default/template/perl/image_thumbnail.pl
+++ default/template/perl/image_thumbnail.pl
@@ -20,7 +20,7 @@ sub ImageMakeThumbnails { # $file
 	if ($fileShellEscaped =~ m/(.+)/) { #todo #security
 		$fileShellEscaped = $1;
 	} else {
-		WriteLog('IndexImageFile: warning: sanity check failed on $fileShellEscaped!');
+		WriteLog('ImageMakeThumbnails: warning: sanity check failed on $fileShellEscaped!');
 		return '';
 	}
 
@@ -30,14 +30,14 @@ sub ImageMakeThumbnails { # $file
 	if ($HTMLDIR =~ m/(.+)/) { #todo #security
 		$HTMLDIR = $1;
 	} else {
-		WriteLog('IndexImageFile: warning: sanity check failed on $HTMLDIR!');
+		WriteLog('ImageMakeThumbnails: warning: sanity check failed on $HTMLDIR!');
 		return '';
 	}
 
 	if ($fileHash =~ m/(.+)/) { #todo #security
 		$fileHash = $1;
 	} else {
-		WriteLog('IndexImageFile: warning: sanity check failed on $fileHash');
+		WriteLog('ImageMakeThumbnails: warning: sanity check failed on $fileHash');
 		return '';
 	}
 
@@ -46,10 +46,10 @@ sub ImageMakeThumbnails { # $file
 	#my @res = qw(800 512 42);
 	if (!-e "$HTMLDIR/thumb/thumb_800_$fileHash.gif") {
 		my $convertCommand = "convert \"$fileShellEscaped\" -auto-orient -thumbnail 800x800 -strip $HTMLDIR/thumb/thumb_800_$fileHash.gif";
-		WriteLog('IndexImageFile: ' . $convertCommand);
+		WriteLog('ImageMakeThumbnails: ' . $convertCommand);
 
 		my $convertCommandResult = `$convertCommand`;
-		WriteLog('IndexImageFile: convert result: ' . $convertCommandResult);
+		WriteLog('ImageMakeThumbnails: convert result: ' . $convertCommandResult);
 
 		#sub DBAddTask { # $taskType, $taskName, $taskParam, $touchTime # make new task
 
@@ -57,24 +57,24 @@ sub ImageMakeThumbnails { # $file
 	if (!-e "$HTMLDIR/thumb/thumb_512_g_$fileHash.gif") {
 		my $convertCommand = "convert \"$fileShellEscaped\" -auto-orient -thumbnail 512x512 -colorspace Gray -blur 0x16 -strip $HTMLDIR/thumb/thumb_512_g_$fileHash.gif";
 		#my $convertCommand = "convert \"$fileShellEscaped\" -scale 5% -blur 0x25 -resize 5000% -colorspace Gray -blur 0x8 -thumbnail 512x512 -strip $HTMLDIR/thumb/thumb_512_$fileHash.gif";
-		WriteLog('IndexImageFile: ' . $convertCommand);
+		WriteLog('ImageMakeThumbnails: ' . $convertCommand);
 
 		my $convertCommandResult = `$convertCommand`;
-		WriteLog('IndexImageFile: convert result: ' . $convertCommandResult);
+		WriteLog('ImageMakeThumbnails: convert result: ' . $convertCommandResult);
 	}
 	if (!-e "$HTMLDIR/thumb/thumb_512_$fileHash.gif") {
 		my $convertCommand = "convert \"$fileShellEscaped\" -auto-orient -thumbnail 512x512 -strip $HTMLDIR/thumb/thumb_512_$fileHash.gif";
-		WriteLog('IndexImageFile: ' . $convertCommand);
+		WriteLog('ImageMakeThumbnails: ' . $convertCommand);
 
 		my $convertCommandResult = `$convertCommand`;
-		WriteLog('IndexImageFile: convert result: ' . $convertCommandResult);
+		WriteLog('ImageMakeThumbnails: convert result: ' . $convertCommandResult);
 	}
 	if (!-e "$HTMLDIR/thumb/thumb_42_$fileHash.gif") {
 		my $convertCommand = "convert \"$fileShellEscaped\" -auto-orient -thumbnail 42x42 -strip $HTMLDIR/thumb/thumb_42_$fileHash.gif";
-		WriteLog('IndexImageFile: ' . $convertCommand);
+		WriteLog('ImageMakeThumbnails: ' . $convertCommand);
 
 		my $convertCommandResult = `$convertCommand`;
-		WriteLog('IndexImageFile: convert result: ' . $convertCommandResult);
+		WriteLog('ImageMakeThumbnails: convert result: ' . $convertCommandResult);
 	}
 } # ImageMakeThumbnails()
 
