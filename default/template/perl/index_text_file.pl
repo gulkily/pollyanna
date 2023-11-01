8fddeaf0eeacc7f164b94fb5ecb521a097c1348c
Ilya Gulko <ilya@ilyagulko.com>
2023-11-01 18:55:02 -0400

    comment out calls to AttachLogToItem(), it is still buggy ; index_text_file.pl, index_zip_file.pl

--- default/template/perl/index_text_file.pl
+++ default/template/perl/index_text_file.pl
@@ -1008,6 +1008,7 @@ sub IndexTextFile { # $file, \%flags | 'flush' ; indexes one text file into data
 										if (!$titleCandidate) {
 											$titleCandidate = '[declined: #' . $tokenFound{'param'} . ']';
 										}
+										#AttachLogToItem($fileHash, 'declined: ' . $tokenFound{'param'} . '; reason: ' . $approveReason);
 										if (GetConfig('admin/index/create_system_tags')) {
 											DBAddLabel($fileHash, 0, 'declined');
 											DBAddLabel('flush');
