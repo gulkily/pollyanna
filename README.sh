eace3a19cae34b63adfcb7918738624b46cb1384
Ilya Gulko <ilya@ilyagulko.com>
2023-11-12 12:05:08 -0500

    add README.sh

---
+++ README.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#
+# Welcome to Pollyanna
+#
+# To get started, run `source hike.sh`
+# This will activate the `hike` command.
+#
+# You can then run `hike help` to see your list of options,
+# such as `hike build`, `hike start`, and `hike clean`.
+#
+#
+cat README.sh
