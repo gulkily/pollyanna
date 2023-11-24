c0200d5f54385d00cef6b263aa9d9ff804c0b299
Ilya Gulko <ilya@ilyagulko.com>
2023-11-24 17:37:12 -0500

    do not show page map dialog if js is off ; page_map.pl

--- default/template/perl/dialog/page_map.pl
+++ default/template/perl/dialog/page_map.pl
@@ -13,6 +13,12 @@ sub GetPageMapDialog {
 # sub GetDialogMenu {
 	WriteLog('GetPageMapDialog()');
 
+    if (GetConfig('setting/js/enable')) {
+        #todo make it work without js
+        WriteLog('GetPageMapDialog: warning: called while setting/js/enable was TRUE; caller = ' . join(' ', caller));
+        return '';
+    }
+
 	my $dialogContent = GetTemplate('html/widget/page_map.template');
 	my $dialog = GetDialogX($dialogContent, 'PageMap');
 
