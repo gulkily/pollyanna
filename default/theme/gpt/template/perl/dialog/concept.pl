b865f6c912321a8222c12c765e67d3da8cfddbba
Ilya Gulko <ilya@ilyagulko.com>
2023-12-14 20:17:36 -0500

    put concept dialog into separate template ; concept.pl (remove from get_page_header.pl)

---
+++ default/theme/gpt/template/perl/dialog/concept.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -T
+
+use strict;
+use warnings;
+use 5.010;
+
+sub GetConceptDialog {
+	my $pageType = shift; # type of page
+
+	my $conceptDialog = '';
+
+	my $conceptInformation = GetString('concept/' . $pageType . '.txt', '', 1);
+	# if that doesn't work, try singular form
+	if (!$conceptInformation) {
+		if (substr($pageType, -1) eq 's') {
+			$conceptInformation = GetString('concept/' . substr($pageType, 0, -1) . '.txt', '', 1);
+		}
+	}
+
+	if ($conceptInformation) {
+		$conceptDialog = '<span class=advanced>' . GetDialogX(ConceptForWeb($conceptInformation), 'Concept') . '</span>';
+	}
+
+	return $conceptDialog;
+} GetConceptDialog()
+
+1;
