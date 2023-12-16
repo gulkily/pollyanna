3386241927241747b9d3abb28b4809ee2aa169a7
Ilya Gulko <ilya@ilyagulko.com>
2023-12-16 14:32:27 -0500

    profile auto register should not trigger on profile or session pages ; profile_auto_register.js

--- default/template/js/profile_auto_register.js
+++ default/template/js/profile_auto_register.js
@@ -9,6 +9,16 @@ function ForceLogin () { // attempts to create a registration
 		// already tried it once
 	}
 	window.ForceLoginTried = 1;
+	// should not trigger on session or profile page
+
+	// we should rewrite this using .indexOf
+	//if (window.location.href.match(/\/session\/|\/profile\//)) {
+	if (window.location.href.indexOf('session.html') != -1 || window.location.href.indexOf('profile.html') != -1) {
+		return '';
+	}
+
+	// we should also set a cookie so that we don't keep prompting the user every session
+	// this just shouldn't be used with openpgp_keygen_prompt_for_username
 	if (window.MakeKey && window.getPublicKey) {
 		if (!getPublicKey()) {
 			MakeKey(); // # this may prompt for a username
