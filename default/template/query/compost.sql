930b3358f47975a872f175767d1ad6ee6bc5d2d6
Ilya Gulko <ilya@ilyagulko.com>
2023-12-30 14:08:16 -0500

    improve query for compost page ; compost.sql

--- default/template/query/compost.sql
+++ default/template/query/compost.sql
@@ -12,6 +12,7 @@ FROM
 	LEFT JOIN author_score ON (item_flat.author_key = author_score.author_key)
 WHERE
 	labels_list NOT LIKE '%notext%'
+	AND add_timestamp <= strftime('%s', 'now', '-7 day')
 ORDER BY
 	child_count ASC,
-	add_timestamp DESC
+	add_timestamp ASC
