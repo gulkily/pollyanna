e6a7ada012093915383a6d3b659ad727508f441a
Ilya Gulko <ilya@ilyagulko.com>
2023-12-30 13:55:51 -0500

    improve output of _dev_clean_html.sh

--- default/template/sh/_dev_clean_html.sh
+++ default/template/sh/_dev_clean_html.sh
@@ -10,6 +10,8 @@
 #echo this script is currently disabled because we are now parsing html files as data, and there is test data i want to keep
 #exit;
 
+echo _dev_clean_html.sh
+
 echo "touch -t 197001010000 html/*.html html/*/*.html html/*/*/*.html"
 touch -t 197001010000 html/*.html html/*/*.html html/*/*/*.html
 
@@ -19,8 +21,8 @@ touch -t 197001010000 html/*.js html/*/*.js html/*/*/*.js
 echo find html -iname '*.html' -type f -exec rm {} \;
 find html -iname '*.html' -type f -exec rm {} \;
 
-echo rm -f cache/b/pages/*
-rm -f cache/b/pages/*
+echo rm -vf cache/b/pages/\*
+rm -fv cache/b/pages/*
 
 #echo find html -iname '*.html' -type f -mtime +5 -exec rm {} \;
 #find html -iname '*.html' -type f -mtime +5 -exec rm {} \;
