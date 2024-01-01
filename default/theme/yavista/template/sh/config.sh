81f8560fefc4b424d5167fdb0764cbd4e9850533
Ilya Gulko <ilya@ilyagulko.com>
2024-01-01 15:57:40 -0600

    improve yavista theme

--- default/theme/yavista/template/sh/config.sh
+++ default/theme/yavista/template/sh/config.sh
@@ -7,6 +7,7 @@ echo 0 > config/setting/html/item_page/toolbox_search
 echo 1 > config/setting/html/monochrome
 echo 1 > config/setting/admin/js/openpgp_keygen_prompt_for_username
 echo 1 > config/setting/admin/js/loading
+echo 0 > config/setting/html/menu_layer_controls
 echo "Pray..." > config/string/en/meditate
 echo "#fff0ef" > config/setting/html/color/background
 echo "#403000" > config/setting/html/color/text
