8e31696c690ae410b2b6d27b54b15220958e03e0
Ilya Gulko <ilya@ilyagulko.com>
2024-01-13 19:39:16 -0500

    save what was in hike.sh before to scratch.sh

--- doc/scratch/scratch.sh
+++ doc/scratch/scratch.sh
@@ -10,4 +10,10 @@
 # check that cut command exists
 # check that git rev-parse HEAD returns a string
 
+    #Matthew 5:37
+    #Let what you say be simply ‘Yes’ or ‘No’; anything more than this comes from evil.
+
+    #echo Rejoice always
+    #echo Pray without ceasing
+    #echo Give thanks in all circumstances
 
