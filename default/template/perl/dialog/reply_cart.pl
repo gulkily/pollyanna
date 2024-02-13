fe83a13fa2b5093773dab2fd5dc9dff722b6a7ff
Ilya Gulko <ilya@ilyagulko.com>
2024-02-13 18:30:45 -0500

    add check for js/enable before adding js events to reply cart template ; reply_cart.pl

--- default/template/perl/dialog/reply_cart.pl
+++ default/template/perl/dialog/reply_cart.pl
@@ -13,20 +13,22 @@ sub GetReplyCartDialog { # uses reply_cart.template and needs reply_cart.js
 
 	my $replyCart = GetTemplate('html/widget/reply_cart.template');
 
-	$replyCart = AddAttributeToTag(
-		$replyCart, 'a id=replyCartInsert', 'onclick',
-		"if (window.insertReplyCart) { return insertReplyCart() + clearReplyCart(); }"
-	);
+	if (GetConfig('setting/admin/js/enable')) {
+		$replyCart = AddAttributeToTag(
+			$replyCart, 'a id=replyCartInsert', 'onclick',
+			"if (window.insertReplyCart) { return insertReplyCart() + clearReplyCart(); }"
+		);
 
-	$replyCart = AddAttributeToTag(
-		$replyCart, 'a id=replyCartClear', 'onclick',
-		"if (window.clearReplyCart) { return clearReplyCart() }"
-	);
+		$replyCart = AddAttributeToTag(
+			$replyCart, 'a id=replyCartClear', 'onclick',
+			"if (window.clearReplyCart) { return clearReplyCart() }"
+		); #todo re-add this control to template and rename function for consistency
 
-	$replyCart = AddAttributeToTag(
-		$replyCart, 'a id=replyCartAddAll', 'onclick',
-		"if (window.ReplyCartAddAll) { return ReplyCartAddAll() }"
-	);
+		$replyCart = AddAttributeToTag(
+			$replyCart, 'a id=replyCartAddAll', 'onclick',
+			"if (window.ReplyCartAddAll) { return ReplyCartAddAll() }"
+		); #todo re-add this control to template
+	}
 
 	my $replyCartDialog = GetDialogX($replyCart, 'Cart');
 	$replyCartDialog = '<span class=advanced>' . $replyCartDialog . '</span>';
