648fbce0297b5ccebeaebf7ae2a8dc5d2c178bc7
Ilya Gulko <ilya@ilyagulko.com>
2024-03-21 23:04:44 -0400

    clean up GetAvatar() and factor out GetAvatarIcon() ; avatar.pl

--- default/template/perl/widget/avatar.pl
+++ default/template/perl/widget/avatar.pl
@@ -90,7 +90,6 @@ sub GetAvatar { # $authorKey ; returns HTML avatar based on author key, using av
 		}
 		{
 			my $themesValue = GetConfig('theme');
-
 			$avatarCachePrefix .= '/' . substr(md5_hex($themesValue), 0, 8);
 			# generate cache prefix using hash of all the enabled themes together
 			# this is still less than ideal because it does not include theme names,
@@ -111,28 +110,19 @@ sub GetAvatar { # $authorKey ; returns HTML avatar based on author key, using av
 
 	WriteLog("GetAvatar($authorKey) ; caller = " . join(',', caller));
 
-	if (1) {
-		if ($avatarMemo{$authorKey}) {
-			WriteLog('GetAvatar: found in %avatarMemo');
-			return $avatarMemo{$authorKey};
-		}
-		my $avCacheFile = GetCache("$avatarCachePrefix/$authorKey");
-		if ($avCacheFile) {
-			$avatarMemo{$authorKey} = $avCacheFile;
-			WriteLog('GetAvatar: found cache, returning: $avatarMemo{$authorKey} = ' . $avatarMemo{$authorKey});
-			return $avatarMemo{$authorKey};
-		}
-		WriteLog('GetAvatar: not found in memo, continuing');
+	if ($avatarMemo{$authorKey}) {
+		WriteLog('GetAvatar: found in %avatarMemo');
+		return $avatarMemo{$authorKey};
 	}
+	my $avCacheFile = GetCache("$avatarCachePrefix/$authorKey");
+	if ($avCacheFile) {
+		$avatarMemo{$authorKey} = $avCacheFile;
+		WriteLog('GetAvatar: found cache, returning: $avatarMemo{$authorKey} = ' . $avatarMemo{$authorKey});
+		return $avatarMemo{$authorKey};
+	}
+	WriteLog('GetAvatar: not found in memo, continuing');
 
 	my $avatar = GetTemplate($avatarTemplate);
-
-	if (GetConfig('html/avatar_icons')) {
-		my $avatarIconTemplate = GetTemplate('html/avatar-icon.template');
-		$avatar = str_replace('<span class=icon></span>', $avatarIconTemplate, $avatar);
-	} else {
-		$avatar = str_replace('<span class=icon></span>', '', $avatar);
-	}
 
 	{
 		# trim whitespace from avatar template
@@ -173,7 +163,7 @@ sub GetAvatar { # $authorKey ; returns HTML avatar based on author key, using av
 			if (!$alias) {
 				#user has no alias for some reason.
 				#not sure how this would happen, except if they generated a key with a blank alias
-				WriteLog('GetAlias: warning: $alias was FALSE; $authorKey = ' . $authorKey . '; caller = ' . join(',', caller));
+				WriteLog('GetAvatar: warning: $alias was FALSE; $authorKey = ' . $authorKey . '; caller = ' . join(',', caller));
 				$alias = 'Guest'; #guest...
 			}
 			$alias = trim($alias);
@@ -196,89 +186,42 @@ sub GetAvatar { # $authorKey ; returns HTML avatar based on author key, using av
 		} # if (%authorItemAttributes)
 
 		if (GetConfig('html/avatar_icons')) {
-			my $color1 = '#' . substr($authorKey, 0, 6);
-			my $color2 = '#' . substr($authorKey, 3, 6);
-			my $color3 = '#' . substr($authorKey, 6, 6);
-			my $color4 = '#' . substr($authorKey, 9, 6);
-			my $color5 = '#' . substr($authorKey, 12, 4) . substr($authorKey, 0, 2);
-			my $color6 = '#' . substr($authorKey, 1, 6);
-			my $color7 = '#' . substr($authorKey, 2, 6);
-			my $color8 = '#' . substr($authorKey, 4, 6);
-			my $color9 = '#' . substr($authorKey, 5, 6);
-			my $colorA = '#' . substr($authorKey, 7, 6);
-			my $colorB = '#' . substr($authorKey, 8, 6);
-
-			$alias = encode_entities2($alias);
-			#$alias = encode_entities($alias, '<>&"');
-
-			if ($alias) {
-				my $char1;
-				my $char2;
-				my $char3;
-
-				$char1 = substr($authorKey, 12, 1);
-				$char2 = substr($authorKey, 13, 1);
-				$char3 = substr($authorKey, 14, 1);
-
-				#
-				$char1 =~ tr/0123456789ABCDEF/)!]#$%^&*(;,.:['/;
-				$char2 =~ tr/0123456789ABCDEF/)!]#$%^&*(;,.:['/;
-				$char3 =~ tr/0123456789ABCDEF/)!]#$%^&*(;,.:['/;
-
-				# $char1 =~ tr/0123456789ABCDEF/abcdefghijklmnop/;
-				# $char2 =~ tr/0123456789ABCDEF/abcdefghijklmnop/;
-				# $char3 =~ tr/0123456789ABCDEF/abcdefghijklmnop/;
-
-				# $char1 = '*';
-				# $char2 = '*';
-				# $char3 = '*';
-
-				$avatar =~ s/\$color1/$color1/g;
-				$avatar =~ s/\$color2/$color2/g;
-				$avatar =~ s/\$color3/$color3/g;
-				$avatar =~ s/\$color4/$color4/g;
-				$avatar =~ s/\$color5/$color5/g;
-				$avatar =~ s/\$color6/$color6/g;
-				$avatar =~ s/\$color7/$color7/g;
-				$avatar =~ s/\$color8/$color8/g;
-				$avatar =~ s/\$color9/$color9/g;
-				$avatar =~ s/\$colorA/$colorA/g;
-				$avatar =~ s/\$colorB/$colorB/g;
-
-				$avatar =~ s/\$alias/$alias/g;
-				$avatar =~ s/\$char1/$char1/g;
-				$avatar =~ s/\$char2/$char2/g;
-				$avatar =~ s/\$char3/$char3/g;
-			}
-			else {
-				$avatar = '';
-			}
+			my $avatarIcon = GetAvatarIcon($authorKey);
+			WriteLog('GetAvatar: $avatarIcon = ' . $avatarIcon);
+			$avatar = str_replace('<span class=icon></span>', $avatarIcon, $avatar);
 		} # GetConfig('html/avatar_icons')
 		else {
-			# no icons
-			$aliasHtmlEscaped = encode_entities2($alias);
-			if ($isVerified) {
-				$aliasHtmlEscaped = '<b><i>'.$aliasHtmlEscaped.'</i></b>';
-			} else {
-				$aliasHtmlEscaped = '<b><i>'.$aliasHtmlEscaped.'</i></b>';
-			}
+			WriteLog('GetAvatar: $avatarIcon not necessary, removing placeholder');
+			$avatar = str_replace('<span class=icon></span>', '', $avatar);
 		}
 	} # if ($authorKey)
 	else {
 		WriteLog('GetAvatar: warning: sanity check failed, $authorKey is false');
-		$avatar = '';
+		$avatar = 'Guest';
 		#option:
 		#$avatar = '(Guest)';
 	}
 
-	$avatar =~ str_replace('$alias', $aliasHtmlEscaped, $avatar);
+	$avatar = str_replace('$alias', $aliasHtmlEscaped, $avatar);
 
+	#todo below, approved status should not necessarily override coloring of username based on status
 	my $colorUsername = '';
 
-	if ($isApproved) {
-		my $checkmark = GetString('widget/checkmark');
-		$avatar = '<b>' . GetAlias($authorKey) . $checkmark . '</b>';
-	} else {
+	if (GetConfig('setting/html/avatar_display_approved_status')) {
+		if ($isApproved) {
+			my $checkmark = GetString('widget/checkmark');
+			$avatar = '<b>' . GetAlias($authorKey) . $checkmark . '</b>';
+		}
+		else {
+			my $hourglass = GetString('widget/hourglass');
+			$avatar = '<b>' . GetAlias($authorKey) . $hourglass . '</b>';
+		}
+	}
+	else {
+		$avatar = '<b>' . GetAlias($authorKey) . '</b>';
+	}
+
+	{
 		if (IsAdmin($authorKey)) {
 			$colorUsername = GetThemeColor('admin_text');
 			WriteLog('GetAvatar: $colorUsername reason is admin');
