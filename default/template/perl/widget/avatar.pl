19c7b65b98f1adea29230747975cd595535c83ce
Ilya Gulko <ilya@ilyagulko.com>
2024-03-21 22:58:47 -0400

    add GetAvatarIcon() in avatar.pl

--- default/template/perl/widget/avatar.pl
+++ default/template/perl/widget/avatar.pl
@@ -5,6 +5,68 @@ use warnings;
 use 5.010;
 use utf8;
 
+sub GetAvatarIcon { # $authorKey ; returns text-based icon for avatar
+	my $authorKey = shift;
+	#todo sanity checks on $authorKey
+	#todo sanity check that setting/html/avatar_icons is enabled
+
+	WriteLog('GetAvatarIcon: $authorKey = ' . $authorKey . '; caller = ' . join(',', caller));
+
+	my $avatarIcon = GetTemplate('html/avatar-icon.template');
+
+	if (!$avatarIcon) {
+		WriteLog('GetAvatarIcon: warning: sanity check failed on $avatarIcon');
+		return '';
+	}
+
+	my $color1 = '#' . substr($authorKey, 0, 6);
+	my $color2 = '#' . substr($authorKey, 3, 6);
+	my $color3 = '#' . substr($authorKey, 6, 6);
+	my $color4 = '#' . substr($authorKey, 9, 6);
+	my $color5 = '#' . substr($authorKey, 12, 4) . substr($authorKey, 0, 2);
+	my $color6 = '#' . substr($authorKey, 1, 6);
+	my $color7 = '#' . substr($authorKey, 2, 6);
+	my $color8 = '#' . substr($authorKey, 4, 6);
+	my $color9 = '#' . substr($authorKey, 5, 6);
+	my $colorA = '#' . substr($authorKey, 7, 6);
+	my $colorB = '#' . substr($authorKey, 8, 6);
+
+	my $char1;
+	my $char2;
+	my $char3;
+
+	$char1 = substr($authorKey, 12, 1);
+	$char2 = substr($authorKey, 13, 1);
+	$char3 = substr($authorKey, 14, 1);
+
+	#
+	$char1 =~ tr/0123456789ABCDEF/)!]#$%^&*(;,.:['/;
+	$char2 =~ tr/0123456789ABCDEF/)!]#$%^&*(;,.:['/;
+	$char3 =~ tr/0123456789ABCDEF/)!]#$%^&*(;,.:['/;
+
+	$avatarIcon =~ s/\$color1/$color1/g;
+	$avatarIcon =~ s/\$color2/$color2/g;
+	$avatarIcon =~ s/\$color3/$color3/g;
+	$avatarIcon =~ s/\$color4/$color4/g;
+	$avatarIcon =~ s/\$color5/$color5/g;
+	$avatarIcon =~ s/\$color6/$color6/g;
+	$avatarIcon =~ s/\$color7/$color7/g;
+	$avatarIcon =~ s/\$color8/$color8/g;
+	$avatarIcon =~ s/\$color9/$color9/g;
+	$avatarIcon =~ s/\$colorA/$colorA/g;
+	$avatarIcon =~ s/\$colorB/$colorB/g;
+
+	$avatarIcon =~ s/\$char1/$char1/g;
+	$avatarIcon =~ s/\$char2/$char2/g;
+	$avatarIcon =~ s/\$char3/$char3/g;
+
+	if (!$avatarIcon) {
+		WriteLog('GetAvatarIcon: warning: $avatarIcon is FALSE!');
+	}
+
+	return $avatarIcon;
+} # GetAvatarIcon()
+
 sub GetAvatar { # $authorKey ; returns HTML avatar based on author key, using avatar.template
 	# sub GetAvatarCache {
 	# affected by config/html/avatar_icons
