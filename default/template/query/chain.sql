1e2cdbaa0183d16553c4be4b2fc51dc733f99e9a
Ilya Gulko <ilya@ilyagulko.com>
2024-03-25 11:49:23 -0400

    update default chain.sql with gpt's version so that it includes chain_hash column ; chain.sql

--- default/template/query/chain.sql
+++ default/template/query/chain.sql
@@ -1,18 +1,22 @@
 SELECT
-	'' AS special_title_labels_list_author,
-	item_flat.item_title item_title,
-	item_flat.author_key AS author_id,
-	item_flat.labels_list labels_list,
-	item_attribute.value AS chain_order,
-	item_attribute.epoch chain_timestamp,
-	item_flat.add_timestamp AS add_timestamp,
-	item_attribute.file_hash AS file_hash,
-	'' AS tagset_chain,
-	'' AS cart
+    '' AS special_title_labels_list_author,
+    item_flat.item_title AS item_title,
+    item_flat.author_key AS author_id,
+    item_flat.labels_list AS labels_list,
+    item_attribute_chain.value AS chain_order,
+    item_attribute_chain.epoch AS chain_timestamp,
+    item_flat.add_timestamp AS add_timestamp,
+    item_attribute_hash.value AS chain_hash,
+    item_attribute_chain.file_hash AS file_hash,
+    '' AS tagset_chain,
+    '' AS cart
 FROM
-	item_attribute
-	JOIN item_flat ON (item_flat.file_hash = item_attribute.file_hash)
+    item_attribute AS item_attribute_chain
+JOIN
+    item_flat ON (item_flat.file_hash = item_attribute_chain.file_hash)
+LEFT JOIN
+    item_attribute AS item_attribute_hash ON (item_attribute_hash.file_hash = item_flat.file_hash AND item_attribute_hash.attribute = 'chain_hash')
 WHERE
-	attribute = 'chain_sequence'
+    item_attribute_chain.attribute = 'chain_sequence'
 ORDER BY
-	chain_timestamp DESC
+    chain_timestamp DESC
