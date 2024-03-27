4f4144c8faadcf214ff260ac160194d375c415d5
Ilya Gulko <ilya@ilyagulko.com>
2024-03-27 09:32:32 -0400

    improve MakeFeed() to include author feeds ; item_listing_page.pl

--- default/template/perl/item_listing_page.pl
+++ default/template/perl/item_listing_page.pl
@@ -200,11 +200,22 @@ sub MakeFeed { # writes a bare-bones txt file with items list
 
 	#todo make templates for this, e.g. template/query/feed/new.sql
 	my $plaintextList = '';
+
+	my $htmlDir = GetDir('html');
+	my $fileOut = "$htmlDir/$feed.txt";
+
 	if ($feed eq 'new') {
 		$plaintextList = SqliteQuery("SELECT file_hash, CAST (add_timestamp AS INT) AS add_timestamp, file_path FROM item_flat ORDER BY add_timestamp DESC LIMIT 20");
 	}
 	elsif ($feed eq 'scores') {
 		$plaintextList = SqliteQuery("SELECT author_key, author_score FROM author_score WHERE author_key ORDER BY author_score DESC LIMIT 100");
+	}
+	elsif ($feed eq 'author') {
+		my $authorKey = shift;
+		if ($authorKey = IsFingerprint($authorKey)) {
+			$plaintextList = SqliteQuery("SELECT file_hash, CAST (add_timestamp AS INT) AS add_timestamp FROM item_flat WHERE author_key = ?", $authorKey);
+			$fileOut = "$htmlDir/author/$authorKey.txt";
+		}
 	}
 	elsif (GetTemplate("query/$feed.sql")) {
 		$plaintextList = SqliteQuery($feed);
@@ -217,9 +228,8 @@ sub MakeFeed { # writes a bare-bones txt file with items list
 	WriteLog('MakeFeed: length($plaintextList) = ' . length($plaintextList));
 
 	if ($plaintextList) {
-		my $htmlDir = GetDir('html');
 		$plaintextList = str_replace($htmlDir, '', $plaintextList); # this is a horrible hack #todo
-		my $fileOut = "$htmlDir/$feed.txt";
+
 		WriteLog('MakeFeed: $fileOut = ' . $fileOut);
 		PutFile($fileOut, $plaintextList);
 	} else {
