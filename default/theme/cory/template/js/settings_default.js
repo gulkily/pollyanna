f5a06923544200afbee8011622b4e3e7c74cdf5d
Ilya Gulko <ilya@ilyagulko.com>
2024-03-31 18:39:51 -0400

    add settings_default.js for cory template

---
+++ default/theme/cory/template/js/settings_default.js
@@ -0,0 +1,39 @@
+// settings_default.js
+
+function GetDefault (prefKey) { // get default value of preference
+// function GetSetting () {
+	if (!prefKey) {
+		//alert('DEBUG: GetDefault: warning: missing prefKey');
+		return '';
+	}
+
+	//alert('DEBUG: GetDefault: prefKey = ' + prefKey);
+
+    // Correctly define the object with key-value pairs
+    var defaultPrefs = {
+        'show_advanced': 0,
+        'beginner': 1,
+        'beginner_highlight': 1,
+        'show_admin': 0,
+        'notify_on_change': 1,
+        'timestamps_format': 'adjusted',
+        'performance_optimization': 'faster',
+        'draggable': 1,
+        'draggable_restore': 1,
+        'draggable_scale': 0,
+        'draggable_arrange_viewport_resize': 0
+    };
+
+    // Check if the prefKey exists in the defaultPrefs object
+    if (defaultPrefs.hasOwnProperty && defaultPrefs.hasOwnProperty(prefKey)) {
+        //alert('DEBUG: GetDefault(' + prefKey + ')' + ' = ' + defaultPrefs[prefKey]);
+        return defaultPrefs[prefKey];
+    }
+    else {
+        //alert('DEBUG: GetDefault: warning: prefKey = ' + prefKey + ' not found in defaultPrefs');
+        return '';
+    }
+} // GetDefault()
+
+// / settings_default.js
+
