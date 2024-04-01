83282deb26fd47f112c4b9d1020241cc0cfc8613
Ilya Gulko <ilya@ilyagulko.com>
2024-04-01 03:29:48 -0400

    add sanity/feature checks and debug output in selectKeyPopulate() profile.js

--- default/template/js/profile.js
+++ default/template/js/profile.js
@@ -586,38 +586,46 @@ function PubKeyPing () { // checks if user's public key is on server
 } // PubKeyPing()
 
 function selectKeyPopulate () {
-	if (document.formSelectKey && document.formSelectKey.selectKey) {
-		if (1 < document.formSelectKey.selectKey.length) {
-			// already populated
-		} else { // #todo put this in event loop?
-			if ((window.SetPrefs) && (window.GetPrefs)) {
-				var iPrivKey = 0;
-				var currentKey = window.localStorage.getItem('privatekey'); // #todo this should use GetPrefs() instead of using localStorage directly
-				//var foundSelected = 0;
-				while (GetPrefs('pk' + iPrivKey, 'PrivateKey1')) {
-					//if (GetPrefs('pk' + iPrivKey, 'PrivateKey1') == keyArmored) {
-					//	return 1; // already stored
-					//}
-					var keyLabel = GetPrefs('pk' + iPrivKey, 'PrivateKeyName') || 'pk' + iPrivKey;
-					var o = new Option(keyLabel, 'pk' + iPrivKey);
-					// it should also add selected attribute to the option if it is the one matching the private key in the 'privatekey' localstorage entry:
-					if (currentKey == GetPrefs('pk' + iPrivKey, 'PrivateKey1')) {
-						o.setAttribute('selected', 'selected');
-						//foundSelected = 1;
+	if (window.localStorage) {
+		if (document.formSelectKey && document.formSelectKey.selectKey) {
+			if (1 < document.formSelectKey.selectKey.length) {
+				// already populated
+			} else { // #todo put this in event loop?
+				if ((window.SetPrefs) && (window.GetPrefs)) {
+					var iPrivKey = 0;
+					var currentKey = window.localStorage.getItem('privatekey'); // #todo this should use GetPrefs() instead of using localStorage directly
+					//var foundSelected = 0;
+					while (GetPrefs('pk' + iPrivKey, 'PrivateKey1')) {
+						//if (GetPrefs('pk' + iPrivKey, 'PrivateKey1') == keyArmored) {
+						//	return 1; // already stored
+						//}
+						var keyLabel = GetPrefs('pk' + iPrivKey, 'PrivateKeyName') || 'pk' + iPrivKey;
+						var o = new Option(keyLabel, 'pk' + iPrivKey);
+						// it should also add selected attribute to the option if it is the one matching the private key in the 'privatekey' localstorage entry:
+						if (currentKey == GetPrefs('pk' + iPrivKey, 'PrivateKey1')) {
+							o.setAttribute('selected', 'selected');
+							//foundSelected = 1;
+						}
+						document.formSelectKey.selectKey.add(o);
+						iPrivKey++;
 					}
-					document.formSelectKey.selectKey.add(o);
-					iPrivKey++;
+					//if (foundSelected) {
+					//	var o = new Option('[LogOut]', 2);
+					//	document.formSelectKey.selectKey.add(o);
+					//} else {
+					//	var o = new Option('[NewAuthor]', 1);
+					//	document.formSelectKey.selectKey.add(o);
+					//}
+					//SetPrefs('pk' + iPrivKey, keyArmored, 'PrivateKey1');
 				}
-				//if (foundSelected) {
-				//	var o = new Option('[LogOut]', 2);
-				//	document.formSelectKey.selectKey.add(o);
-				//} else {
-				//	var o = new Option('[NewAuthor]', 1);
-				//	document.formSelectKey.selectKey.add(o);
-				//}
-				//SetPrefs('pk' + iPrivKey, keyArmored, 'PrivateKey1');
 			}
 		}
+		else {
+			//alert('DEBUG: selectKeyPopulate: document.formSelectKey.selectKey check FAILED');
+		}
+	}
+	else {
+		//alert('DEBUG: selectKeyPopulate: window.localStorage check FAILED');
 	}
 
 	//alert(document.formSelectKey.selectKey.value);
