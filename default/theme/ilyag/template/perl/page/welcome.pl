c8f30a15e9136a5644339ed5f954d90c18a38b97
Ilya Gulko <ilya@ilyagulko.com>
2024-04-25 14:54:03 -0400

    clean up welcome.pl for ilyag theme

--- default/theme/ilyag/template/perl/page/welcome.pl
+++ default/theme/ilyag/template/perl/page/welcome.pl
@@ -6,25 +6,6 @@ use 5.010;
 
 sub GetWelcomePage {
 	WriteLog('GetWelcomePage: ilyag theme');
-## this welcome page displays all items which have a tag of 'approve' and a score of greater than 0
-#	my %params;
-#	$params{'where_clause'} = "WHERE labels_list LIKE '%approve%' AND item_score > 0";
-#	my @files = DBGetItemList(\%params);
-#
-#	my $welcomePage =
-#		GetPageHeader('welcome') .
-#		GetItemListHtml(\@files) .
-#		GetPageFooter('welcome')
-#	;
-#
-#	if (GetConfig('admin/js/enable')) {
-#		my @js = qw(avatar puzzle settings profile utils timestamp clock fresh table_sort voting write);
-#		if (GetConfig('admin/php/enable')) {
-#			push @js, 'write_php'; # write.html
-#		}
-#		$welcomePage = InjectJs($welcomePage, @js)
-#	}
-#	return $welcomePage
 
 	my $html = GetTemplate('html/page/home.template');
 
