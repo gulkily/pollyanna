a1b796e714448e0182565e59cbe263ce8c9f3103
Ilya Gulko <ilya@ilyagulko.com>
2024-04-25 15:40:06 -0400

    improve ilyag theme home/welcome page

--- default/theme/ilyag/template/perl/page/welcome.pl
+++ default/theme/ilyag/template/perl/page/welcome.pl
@@ -22,6 +22,9 @@ sub GetWelcomePage {
 	if (file_exists("$IMAGEDIR/spiritual_computing_combined.pdf")) {
 		$html = str_replace('<a>Hacker\'s Guide to Spiritual Computing</a>', '<a href="/image/spiritual_computing_combined.pdf">Hacker\'s Guide to Spiritual Computing</a>', $html);
 	}
+	if (file_exists("$IMAGEDIR/whitepaper-pollyanna.pdf.pdf")) {
+		$html = str_replace('<a>Pollyanna whitepaper</a>', '<a href="/image/whitepaper-pollyanna.pdf">Pollyanna whitepaper</a>', $html);
+	}
 
 	if (GetConfig('admin/image/enable')) {
 		my $image = SqliteGetValue("SELECT file_hash FROM item_flat WHERE item_type = 'image' AND labels_list LIKE '%,welcome,%' AND item_score > 0 ORDER BY RANDOM() LIMIT 1");
