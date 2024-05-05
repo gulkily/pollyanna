4f7ae6b6230621020084436bebf352239982a721
Ilya Gulko <ilya@ilyagulko.com>
2024-05-05 17:17:27 -0400

    fix error in find_bots.sql (thanks, chatgpt)

--- default/template/query/find_bots.sql
+++ default/template/query/find_bots.sql
@@ -7,18 +7,18 @@ SELECT
 	remote_addr_ip_log.first_three_octets,
 	remote_addr_ip_log.first_two_octets,
 	item_flat.item_title AS item_title,
-	ABS(add_timestamp - chain_timestamp) AS time_diff
+	ABS(item_flat.add_timestamp - item_attribute_chain.epoch) AS time_diff
 FROM
 	item_attribute AS item_attribute_chain
 JOIN
-	item_flat ON (item_flat.file_hash = item_attribute_chain.file_hash)
+	item_flat ON item_flat.file_hash = item_attribute_chain.file_hash
 LEFT JOIN
-	item_attribute AS item_attribute_hash ON (item_attribute_hash.file_hash = item_flat.file_hash AND item_attribute_hash.attribute = 'chain_hash')
+	item_attribute AS item_attribute_hash ON item_attribute_hash.file_hash = item_flat.file_hash AND item_attribute_hash.attribute = 'chain_hash'
 JOIN
-	remote_addr_ip_log ON (remote_addr_ip_log.file_hash = item_flat.file_hash)
+	remote_addr_ip_log ON remote_addr_ip_log.file_hash = item_flat.file_hash
 WHERE
 	item_attribute_chain.attribute = 'chain_sequence' AND
-	time_diff > 60
+	ABS(item_flat.add_timestamp - item_attribute_chain.epoch) > 60
 ORDER BY
 	time_diff DESC,
 	chain_timestamp DESC
