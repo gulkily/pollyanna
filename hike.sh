5763af4c9eea80d12b825e40fa22003c586e6b26
Ilya Gulko <ilya@ilyagulko.com>
2024-05-04 23:46:01 -0400

    remove problematic code from hike.sh, move to scratch.sh

--- hike.sh
+++ hike.sh
@@ -7,40 +7,6 @@
 # set -x
 # uncomment for debugging
 
-####################################################################################################
-# This script is designed to be run from the root of the project directory.
-# It will create an alias for the "hike" command which will change to the project directory
-# and run the script from there. This is useful for running the script from anywhere on the system.
-# The alias is written to the script itself, so it will be available the next time the script is run.
-# It should also add this command to the .bashrc file so that it is available every time a new terminal is opened.
-# Get the current directory
-current_dir=$(pwd)
-
-# Check if HOME variable is set
-if [ -n "$HOME" ]; then
-  # Replace home directory path with ~ if current directory is under home
-  current_dir="${current_dir/#$HOME/\~}"
-else
-  echo "HOME variable is not defined. Please make sure it is set."
-  exit 1
-fi
-
-# Remove any line which begins with "alias"
-sed -i.bak '/^alias/ d' hike.sh
-
-# Write the new alias line to the script
-echo "alias hike='cd \"$current_dir\" ; bash \"\$PWD/hike.sh\"'" >> hike.sh
-
-## Also add to the .bashrc file and .profile file if not already there
-#if ! grep -q "alias hike='cd \"$current_dir\" ; bash \"\$PWD/hike.sh\"'" ~/.bashrc; then
-#  echo "alias hike='cd \"$current_dir\" ; bash \"\$PWD/hike.sh\"'" >> ~/.bashrc
-#fi
-#if ! grep -q "alias hike='cd \"$current_dir\" ; bash \"\$PWD/hike.sh\"'" ~/.profile; then
-#  echo "alias hike='cd \"$current_dir\" ; bash \"\$PWD/hike.sh\"'" >> ~/.profile
-#fi
-
-####################################################################################################
-
 if [ ! $1 ]
 	then
 		set 1=help
