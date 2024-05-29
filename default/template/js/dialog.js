a5ccd42105e38a603ad49252ad87868a50604527
Ilya Gulko <ilya@ilyagulko.com>
2024-05-29 15:39:17 -0400

    add GetTimestampWidget() and CreateItemDialog() basic and test ; dialog.js

--- default/template/js/dialog.js
+++ default/template/js/dialog.js
@@ -34,4 +34,55 @@ function CreateDialog (body, title, status) {
 	document.body.appendChild(newDialog);
 } // CreateDialog()
 
+function GetTimestampWidget (time) {
+	/* timestamp_time.template */
+
+	return '<time class=timestamp datetime=' + time + ' title="' + 'todo' + '">' + 'todo' + '</time>';
+}
+
+function CreateItemDialog (file) {
+
+	/*
+	# %file(hash for each file)
+	# file_path = file path including filename
+	# file_hash = git's hash of the file's contents
+	# author_key = gpg key of author (if any)
+	# add_timestamp = time file was added as unix_time
+	# child_count = number of replies
+	# display_full_hash = display full hash for file
+	# template_name = item/item.template by default
+	# remove_token = token to remove (for reply tokens)
+	# show_vote_summary = shows item's list and count of tags
+	# show_quick_vote = displays quick vote buttons
+	# item_title = override title
+	# item_statusbar = override statusbar
+	# labels_list = comma-separated list of tags the item has
+	# is_textart = set <tt><code> tags for the message itself
+	# no_permalink = do not link to item's permalink page
+	*/
+
+	/* #todo sanity checks */
+
+	var status = GetTimestampWidget(testItem.add_timestamp);
+
+	var body = escapeHTML(file.body);
+	body = body.replace("\n", "<br>");
+
+	var title = escapeHTML(file.item_title);
+
+	CreateDialog(body, title, status);
+}
+
+/*
+var testItem = {};
+testItem.item_title = 'this is a test item, thanks';
+testItem.labels_list = 'hastext,hastitle';
+testItem.hash = 'abcdef012346789abcdef012346789abcdef012346789abcd';
+testItem.path = '/txt/ab/cd/abcdef012346789abcdef012346789abcdef012346789abcd.txt';
+testItem.body = 'asdfasdfd' + "\n" + "\n" + "\n" + 'tefasdfads';
+testItem.add_timestamp = 1716921560;
+
+CreateItemDialog(testItem);
+*/
+
 /* / dialog.js */
