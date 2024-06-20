ee87bca7f47be836c2ae190f67dfba4ea5181247
Ilya Gulko <ilya@ilyagulko.com>
2024-06-20 19:22:06 -0400

    improve cookie/session notice printed by route.php

--- default/template/php/route.php
+++ default/template/php/route.php
@@ -996,7 +996,7 @@ if (GetConfig('setting/admin/php/route_enable')) {
 						$cookieNotice = 'Cookie: ' . $currentCookie . $cookieLookup;
 
 						$cookieNotice = '<span class=advanced>' . $cookieNotice . '</span>';
-						$cookieNotice = '<a href="/session.html">You are signed in with a cookie.</session>';
+						$cookieNotice = '<a href="/session.html">You are signed in with a cookie.</a><br>' . "\n" . $cookieNotice;
 
 						$cookieNotice = AddAttributeToTag(GetDialogX($cookieNotice, 'CookieInfo', '', '', ''), 'table', 'id', 'CookieInfo');
 
